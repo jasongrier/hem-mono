5ab9454c46dbc3924101fbe8f135639b631eb56f
Jason Grier <jason@hemberlin.de>
2019-06-24 23:30:26 +0200

    WIP filtering content from project

--- src/sidebar/container.tsx
+++ src/sidebar/container.tsx
@@ -1,14 +1,20 @@
 import React, { ReactElement, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import useWindowSize from '@rehooks/window-size'
 import { RootState } from '../store'
+import { updateContent } from './redux'
 import View from './view'
 
 function SidebarContainer(): ReactElement {
   const { filteredTags, filteredFiles } = useSelector((state: RootState) => ({
-    filteredTags: state.sidebar.filteredTags,
+    allFiles: state.project.files,
+    allTags: state.project.tags,
     filteredFiles: state.sidebar.filteredFiles,
+    filteredTags: state.sidebar.filteredTags,
   }))
+
+  const dispatch = useDispatch()
+
+  useEffect(() => { dispatch(updateContent(allFiles, allTags)) }, [allFiles, allTags])
 
   return (
     <View
