1c9f011a1be304efc19044c0d8057c5b120c632d
Jason Grier <jason@hemberlin.de>
2019-10-05 16:01:22 -0500

    App page styles

--- src/projects/sites/midst-journal/routes/AboutTheApp.tsx
+++ src/projects/sites/midst-journal/routes/AboutTheApp.tsx
@@ -1,28 +1,19 @@
-import React, { ReactElement, useEffect } from 'react'
-import $ from 'jquery'
+import React, { ReactElement } from 'react'
 
 function TheApp(): ReactElement {
-  useEffect(() => {
-    // setTimeout(() => {
-      $('video').prop('volume', 0)
-    // }, 100)
-  }, [])
-
   return (
     <div className="app-page">
       <section className="heroine heroine--normal center">
         <p>
-          Our custom-built word processor privately captures the writing process, <br />
-          allowing you to generate timelapses and replay what you've written.<br />
-          Documents are saved with a .midst extension (instead of .doc, .rtf, .txt, etc.). <br />
-          The Midst app is the engine that lets the journal run. It currently runs on any Mac computer. <br /><br />
-          We're currenty looking for beta testers!<br />
-          Poets interesting in participating in the Midst pilot issue: <br />
-          contact Annelyse at midsthq@gmail.com (subject line BETA).
+          The Midst software is a beautiful, minimalist, custom-built word processor that allows writers to generate interactive timelines of their process.
+          Keep your timelines to yourself or submit them to the Midst journal.
         </p>
-        <video width="500px" style={{border: '5px solid'}} controls loop>
-          <source src="http://data.midst.press/Wheelbarrow.m4v" type="video/mp4" />
-        </video>
+        <p>
+          Sign up below for updates to find out when the app is available!
+        </p>
+        <p>
+          <img src="http://annelysegelman.com/donotdelete/theredwheelbarrow.png" />
+        </p>
       </section>
     </div>
   )
