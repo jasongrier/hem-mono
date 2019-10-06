2682f4d436c7b95081345d979f474953f7da0a32
Jason Grier <jason@hemberlin.de>
2019-10-06 15:06:01 -0500

    Seurat: Halfway ok pattern for bottom buttons

---
+++ src/projects/apps/seurat/components/ControlPanel.tsx
@@ -0,0 +1,61 @@
+import React, { ReactElement } from 'react'
+
+function ControlPanel(): ReactElement {
+  return (
+    <div className="control-panel">
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button" style={{opacity: 0}}>
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button" style={{opacity: 0}}>
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button" style={{opacity: 0}}>
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button" style={{opacity: 0}}>
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button" style={{opacity: 0}}>
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+      <button className="control-panel__button">
+        <i className="fas fa-drum"></i>
+      </button>
+    </div>
+  )
+}
+
+export default ControlPanel
