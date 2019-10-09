d0b1293fbe1c24c5d43c94d4a787dddbc44de212
Annelyse Gelman <annelyse.robin@gmail.com>
2019-10-09 12:34:25 -0500

    New app page.

--- src/projects/sites/midst-journal/routes/AboutTheApp.tsx
+++ src/projects/sites/midst-journal/routes/AboutTheApp.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from 'react'
 function TheApp(): ReactElement {
   return (
     <div className="app-page">
-      <section className="heroine heroine--normal center">
+      <section className="heroine">
         <p>
           The Midst software is a beautiful, minimalist, custom-built word processor that allows writers to generate interactive timelines of their process.
           Keep your timelines to yourself or submit them to the Midst journal.
@@ -11,9 +11,11 @@ function TheApp(): ReactElement {
         <p>
           Sign up below for updates to find out when the app is available!
         </p>
+  
         <p>
           <img src="http://annelysegelman.com/donotdelete/theredwheelbarrow.png" />
         </p>
+
       </section>
     </div>
   )
