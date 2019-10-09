499a86f9833150e906c78de21b657706097d694a
Annelyse Gelman <annelyse.robin@gmail.com>
2019-10-09 10:38:28 -0500

    Home/TOC tweaks

--- src/projects/sites/midst-journal/routes/Home.tsx
+++ src/projects/sites/midst-journal/routes/Home.tsx
@@ -7,10 +7,10 @@ function Home(): ReactElement {
         <p className="first">
           What if you could watch your favorite poet write?
         </p>
-
+{/* 
         <p style={{ color: 'darkred', paddingTop: '20px' }}>
           <a href="http://midst.press/read">Let’s find out.</a>
-        </p>
+        </p> */}
 
         <p style={{ paddingTop: '20px' }}>
           Midst is a new digital journal publishing poems in the form of interactive timelapses. You’ll see the finished text by default, but then you can rewind it to see exactly how it was written: start to finish, blank page to final draft, and every edit in between.
