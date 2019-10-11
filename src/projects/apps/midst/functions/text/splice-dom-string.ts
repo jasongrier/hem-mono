2467b7669e89759937de44f63738583c1b49c271
Jason Grier <jason@hemberlin.de>
2019-10-11 18:57:04 -0500

    Midst app: Simple test for editor Redux update lag

---
+++ src/projects/apps/midst/functions/text/splice-dom-string.ts
@@ -0,0 +1,13 @@
+/**
+ * Splice content into subject at a given "real" index, ignoring HTML tags.
+ * (Only the HTML tags that are used in the app...)
+ * @param index
+ * @param content
+ */
+function spliceDomString(index: number, subject: string, content: string) {
+  for (let i = 0; i < subject.length; i++) {
+
+  }
+}
+
+export default spliceDomString
--- src/projects/apps/midst/store/actions.ts
+++ src/projects/apps/midst/store/actions.ts
@@ -1,13 +1,17 @@
 import { AnyAction } from 'redux'
 import {
-  SOME_ACTION,
+  INSERT_LINE,
+  REMOVE_LINE,
+  UPDATE_LINE,
+
+  TMP_UPDATE_CONTENT,
 } from './types'
 
-const someAction = (): AnyAction => ({
-  type: SOME_ACTION,
-  payload: null,
+const tmpUpdateContent = (content: string): AnyAction => ({
+  type: TMP_UPDATE_CONTENT,
+  payload: content,
 })
 
 export {
-  someAction,
+  tmpUpdateContent,
 }
