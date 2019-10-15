19cfd34dea53d8ad4ec9e7adc8ed2e8e77f36f1f
Jason Grier <jason@hemberlin.de>
2019-10-15 16:03:43 -0500

    Seurat: Discovered Tone sampler won't work with note numbers, only letter-based notes

--- src/projects/apps/seurat/functions/create-web-version-sampler.ts
+++ src/projects/apps/seurat/functions/create-web-version-sampler.ts
@@ -2,11 +2,23 @@ import { reduce } from 'lodash'
 const Tone = require('tone') // No @types!!
 const config = require('../config')
 
+const aTwoMp = require('../assets/sounds/A2.mp3')
+const aTwoOg = require('../assets/sounds/A2.ogg')
+
 function createWebVersionSamplerFromTonePlayer(sounds: string[]) {
+  var ampEnv = new Tone.AmplitudeEnvelope({
+    attack: 0.5,
+    decay: 1.0,
+    sustain: 1.0,
+    release: 1.0
+  }).toMaster()
+
   return reduce(sounds, (acc: any, sound: string, index: number) => {
-    const sampler = new Tone.Player(sound).toMaster()
-    // sampler.fadeIn = 0.125
-    // sampler.fadeOut = 0.125
+    const sampler = {
+      // sampler: new Tone.Player(sound).toMaster(),
+      sampler: new Tone.Player(sound).connect(ampEnv),
+      envelope: ampEnv,
+    }
     acc[index + 1] = sampler
     return acc
   }, [])
@@ -18,10 +30,9 @@ function createWebVersionSamplerFromToneSampler(sounds: string[]) {
     return acc
   }, {})
 
-  console.log(soundAssignments)
-
   return new Tone.Sampler({
-    ...soundAssignments,
+    // ...soundAssignments,
+    'A2': aTwoMp,
   }).toMaster()
 }
 
@@ -32,8 +43,13 @@ function createWebVersionSampler(sounds: string[]) {
   if (config.SAMPLER_TYPE === 'player') {
     const sampler = createWebVersionSamplerFromTonePlayer(sounds)
 
+    setTimeout(() => {
+      sampler[1].sampler.start()
+    }, 500)
+
     playFn = function(noteNumber: number) {
-      sampler[noteNumber].start()
+      sampler[noteNumber].envelope.triggerRelease()
+      sampler[noteNumber].envelope.triggerAttackRelease()
     }
 
     stopFn = function(noteNumber: number) {
@@ -45,7 +61,7 @@ function createWebVersionSampler(sounds: string[]) {
     const sampler = createWebVersionSamplerFromToneSampler(sounds)
 
     playFn = function(noteNumber: number) {
-      sampler.triggerAttack(noteNumber) // TODO: Why is this repitching when the note numbers match??
+      sampler.triggerAttack('A2') // TODO: Why is this repitching when the note numbers match??
     }
 
     stopFn = function(noteNumber: number) {
