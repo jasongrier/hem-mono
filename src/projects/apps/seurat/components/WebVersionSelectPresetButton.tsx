d67e7b059b1ee5183c0b1aea4368a8810478ec45
Jason Grier <jason@hemberlin.de>
2019-10-20 17:50:02 -0500

    Seurat: Web version insts are just canvas presets

--- src/projects/apps/seurat/components/WebVersionSelectPresetButton.tsx
+++ src/projects/apps/seurat/components/WebVersionSelectPresetButton.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../store'
+import { setCanvas } from '../store/actions'
 import ControlPanelButton from './ControlPanelButton'
 
 interface IProps {
@@ -8,17 +9,31 @@ interface IProps {
   type: string
 }
 
+const activeClassMap = [
+  'amp',
+  'bells',
+  'drum',
+  'guitars',
+  'piano',
+  'sax',
+  'turntable',
+]
+
 function WebVersionSelectPresetButton({ className, type }: IProps): ReactElement {
+  const { currentCanvas } = useSelector((state: RootState) => ({
+    currentCanvas: state.app.currentCanvas,
+  }))
+
   const dispatch = useDispatch()
 
   return (
     <ControlPanelButton
       className={
         (className ? className : '')
-        + (webVersionBoardPreset === type ? ' selected' : '')
+        + (activeClassMap[currentCanvas] === type ? ' selected' : '')
       }
       icon={type}
-      onClick={() => dispatch(setWebVersionPreset(type))}
+      onClick={() => dispatch((setCanvas(activeClassMap.indexOf(type))))}
     />
   )
 }
