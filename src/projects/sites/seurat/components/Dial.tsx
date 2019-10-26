548f019092a091760cdb9ecaef4ee49055a69974
Jason Grier <jason@hemberlin.de>
2019-10-26 13:23:42 -0500

    Seurat: Cheesy divot to mimmic a painter's palette

--- src/projects/sites/seurat/components/Dial.tsx
+++ src/projects/sites/seurat/components/Dial.tsx
@@ -1,15 +1,40 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, SyntheticEvent } from 'react'
 
 interface IProps {
   controlled: boolean
+  label: string
   onChange: (value: number) => void
   onChangeDone: (value: number) => void
   value: number
 }
 
-function Dial({ controlled, onChange, onChangeDone, value }: IProps): ReactElement {
+function normalizedValue(value: number) {
+  return (value + 45) / 90 * 100
+}
+
+function denormalizedValue(value: number) {
+  return value / 100 * 90 -45
+}
+
+function Dial({ controlled, label, onChange, onChangeDone, value }: IProps): ReactElement {
+
+  function onMouseDown(evt: SyntheticEvent<HTMLDivElement>) {
+
+  }
+
   return (
-    <div className="dial">
+    <div className={`dial ${label ? ' dial--with-label' : ''}`}>
+      <div
+        className="dial__inner"
+        onMouseDown={onMouseDown}
+        style={{
+          transform: `rotate(${value / 100 * 360}deg)`,
+        }}
+      >
+        <div className="dial__label">
+          {Math.round(normalizedValue(value))}
+        </div>
+      </div>
     </div>
   )
 }
