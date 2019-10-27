37b9e1038f31c4588837a60c98c044435c4392fd
Jason Grier <jason@hemberlin.de>
2019-10-26 20:04:04 -0500

    Seurat: Start prep for hardware parity

--- src/projects/sites/seurat/components/Palette.tsx
+++ src/projects/sites/seurat/components/Palette.tsx
@@ -5,7 +5,7 @@ import PerformanceControls from './PerformanceControls'
 function Palette(): ReactElement {
   return (
     <div className="palette">
-      <PerformanceControls cursorGroup="white" />
+      {/* <PerformanceControls cursorGroup="white" /> */}
       <PerformanceControls cursorGroup="red" />
       <PerformanceControls cursorGroup="yellow" />
       <PerformanceControls cursorGroup="blue" />
