ce392bfbfb1f3229bf20fb8de4f6ffe90d691d67
Jason Grier <jason@hemberlin.de>
2019-10-31 11:19:24 -0500

    Seurat: 'LCD' screen

--- src/projects/sites/seurat/components/LcdScreen.tsx
+++ src/projects/sites/seurat/components/LcdScreen.tsx
@@ -1,23 +1,28 @@
 import React, { ReactElement } from 'react'
+import uuid from 'uuid/v1'
 
 interface IProps {
   content: string
 }
 
-const letterPatternsCompressed = 'oxxxxxo|xooooox|xooooox|xxxxxxx|xooooox|xooooox|xooooox=|xxxxxxo|xooooox|xooooox|xxxxxxo|xooooox|xooooox|xxxxxxo=|oxxxxxx|xoooooo|xoooooo|xoooooo|xoooooo|xoooooo|oxxxxxx=|xxxxxxo|xooooox|xooooox|xooooox|xooooox|xooooox|xxxxxxo=|xxxxxxx|xoooooo|xoooooo|xxxxooo|xoooooo|xoooooo|xxxxxxx=|xxxxxxx|xoooooo|xoooooo|xxxxooo|xoooooo|xoooooo|xoooooo=|oxxxxxo|xoooooo|xoooooo|xoooooo|xooooxx|xooooox|oxxxxxo=|xooooox|xooooox|xooooox|xxxxxxx|xooooox|xooooox|xooooox=|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo=|oooooox|oooooox|oooooox|oooooox|xooooox|xooooox|oxxxxxo=|xooooox|xooooxo|xoooxoo|xxxxooo|xoooxoo|xooooxo|xooooox=|xoooooo|xoooooo|xoooooo|xoooooo|xoooooo|xoooooo|xxxxxxx=|xooooox|xxoooxx|xoxoxox|xooxoox|xooooox|xooooox|xooooox=|xooooox|xxoooox|xoxooox|xooxoox|xoooxox|xooooxx|xooooox=|oxxxxxo|xooooox|xooooox|xooooox|xooooox|xooooox|oxxxxxo=|xxxxxxo|xooooox|xooooox|xxxxxxo|xoooooo|xoooooo|xoooooo=|oxxxxxo|xooooox|xooooox|xooooox|xoxooxo|xooxxxo|oxxooxx=|xxxxxxo|xooooox|xooooox|xxxxxxo|xooooox|xooooox|xooooox=|oxxxxxx|xoooooo|xoooooo|oxxxxxo|oooooox|oooooox|xxxxxxo=|xxxxxxx|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo=|xooooox|xooooox|xooooox|xooooox|xooooox|xooooox|oxxxxxo=|xooooox|xooooox|xooooox|xooooox|xooooox|oxoooxo|ooxxxoo=|xooooox|xooooox|xooooox|xooxoox|x x x x|xxoooxx|xooooox=|xooooox|oxoooxo|ooxoxoo|oooxooo|ooxoxoo|oxoooxo|xooooox=|xooooox|oxoooxo|ooxoxoo|oooxooo|oooxooo|oooxooo=|xxxxxxx|oooooxo|ooooxoo|oooxooo|ooxoooo|oxooooo|xxxxxxx'
-const letterPatterns = letterPatternsCompressed.split('=').map(l => l.split('|'))
-const letterKeys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const charPatternsCompressed = 'oxxxxxo|xooooox|xooooox|xxxxxxx|xooooox|xooooox|xooooox=xxxxxxo|xooooox|xooooox|xxxxxxo|xooooox|xooooox|xxxxxxo=oxxxxxx|xoooooo|xoooooo|xoooooo|xoooooo|xoooooo|oxxxxxx=xxxxxxo|xooooox|xooooox|xooooox|xooooox|xooooox|xxxxxxo=xxxxxxx|xoooooo|xoooooo|xxxxooo|xoooooo|xoooooo|xxxxxxx=xxxxxxx|xoooooo|xoooooo|xxxxooo|xoooooo|xoooooo|xoooooo=oxxxxxo|xoooooo|xoooooo|xoooooo|xooooxx|xooooox|oxxxxxo=xooooox|xooooox|xooooox|xxxxxxx|xooooox|xooooox|xooooox=oooxooo|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo=oooooox|oooooox|oooooox|oooooox|xooooox|xooooox|oxxxxxo=xooooox|xooooxo|xoooxoo|xxxxooo|xoooxoo|xooooxo|xooooox=xoooooo|xoooooo|xoooooo|xoooooo|xoooooo|xoooooo|xxxxxxx=xooooox|xxoooxx|xoxoxox|xooxoox|xooooox|xooooox|xooooox=xooooox|xxoooox|xoxooox|xooxoox|xoooxox|xooooxx|xooooox=oxxxxxo|xooooox|xooooox|xooooox|xooooox|xooooox|oxxxxxo=xxxxxxo|xooooox|xooooox|xxxxxxo|xoooooo|xoooooo|xoooooo=oxxxxxo|xooooox|xooooox|xooooox|xoxooxo|xooxxxo|oxxooxx=xxxxxxo|xooooox|xooooox|xxxxxxo|xooooox|xooooox|xooooox=oxxxxxx|xoooooo|xoooooo|oxxxxxo|oooooox|oooooox|xxxxxxo=xxxxxxx|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo|oooxooo=xooooox|xooooox|xooooox|xooooox|xooooox|xooooox|oxxxxxo=xooooox|xooooox|xooooox|xooooox|xooooox|oxoooxo|ooxxxoo=xooooox|xooooox|xooooox|xooxoox|x x x x|xxoooxx|xooooox=xooooox|oxoooxo|ooxoxoo|oooxooo|ooxoxoo|oxoooxo|xooooox=xooooox|oxoooxo|ooxoxoo|oooxooo|oooxooo|oooxooo=xxxxxxx|oooooxo|ooooxoo|oooxooo|ooxoooo|oxooooo|xxxxxxx'
+const charPatterns = charPatternsCompressed.split('=').map(l => l.split('|').map(r => r.split('').map(p => p === 'x')))
+const charKeys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-function letter(letter: string) {
+function renderChar(char: string) {
   const grid = []
-  for (let r = 0; r < 4; r ++) {
-    for (let c = 0; c < 3; c ++) {
-      const glyph = letterPatterns[letterKeys.indexOf(letter)]
+  const glyph = charPatterns[charKeys.indexOf(char)]
+
+  for (let r = 0; r < 7; r ++) {
+    for (let c = 0; c < 7; c ++) {
       grid.push(
-        <div className={`
+        <div
+          key={uuid()}
+          className={`
           lcd-screen__pixel
-            lcd-screen__pixel--${glyph && glyph[r + c] ? 'on' : 'off'}
-        `} />
+            lcd-screen__pixel--${glyph && glyph[r] && glyph[r][c] ? 'on' : 'off'}
+          `}
+        />
       )
     }
   }
@@ -27,7 +32,16 @@ function letter(letter: string) {
 function LcdScreen({ content }: IProps): ReactElement {
   return (
     <div className="lcd-screen">
-      {content.split('').map(letter)}
+      <div className="lcd-screen__chars">
+        {content.split('').map(char => (
+          <div
+            key={uuid()}
+            className="lcd-screen__char"
+          >
+            {renderChar(char)}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/projects/sites/seurat/functions/canvas/create-canvas.ts
+++ src/projects/sites/seurat/functions/canvas/create-canvas.ts
@@ -1,11 +1,23 @@
-import { ICanvas, IControls, IDot, IGroupControls } from '../../store/types'
+import { merge } from 'lodash'
+
+// TODO: random-words @types
+//@ts-ignore
+import randomWords from 'random-words'
+import { ICanvas, IControls, IDot, CursorGroup } from '../../store/types'
+
+const randomWordsConfig = {
+  exactly: 1,
+  wordsPerString: 2,
+  maxLength: 5,
+}
 
 function createCanvas(
+  cursorGroup: CursorGroup,
   name: string,
   sound?: string,
 ): ICanvas {
   const defaultDot: IDot = {
-    cursorGroup: 'none',
+    cursorGroup,
     sound,
   }
 
@@ -29,6 +41,22 @@ function createCanvas(
     dots: new Array(64).fill(defaultDot),
     name,
   }
+}
+
+export function createRandomCanvas(dots: boolean = true, name: boolean = true, sound: boolean = true) {
+  return createCanvas(
+    dots ? ['a', 'b', 'c', 'none'][Math.floor(Math.random() * 3)] as CursorGroup : 'none', // TODO: This makes every single dot the same random color!
+    name ? randomWords(randomWordsConfig)[0].toUpperCase() : 'UNTITLED',
+    sound ? 'empty' : 'empty', // TODO: Sound impl.
+  )
+}
+
+export function createRandomCanvases(amount: number) {
+  const canvases: ICanvas[] = []
+  for (var i = 0; i < amount; i++) {
+    canvases.push(createRandomCanvas(false))
+  }
+  return canvases
 }
 
 export default createCanvas
