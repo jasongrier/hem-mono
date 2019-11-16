c3f019a8db91c978a730f40a8a39ccd9a15a3a13
Jason Grier <jason@hemberlin.de>
2019-11-16 00:09:54 -0600

    Studio: Basic tree movie (without Levenshtein calc)

--- src/projects/sites/studio/components/FlipBook.tsx
+++ src/projects/sites/studio/components/FlipBook.tsx
@@ -3,39 +3,33 @@ import React, { ReactElement, useEffect, useState } from 'react'
 type flickerFunction = (frameNumber: number) => boolean
 
 interface IProps {
+  frames: string[] // TODO: All projects, separate alphabetized required props from optionals
+
   endFrame?: number
   flickerMax?: number
   flickerMin?: number
   flickerThreshold?: flickerFunction | number
   frameRate?: number
-  frameType?: 'jpg' | 'svg'
-  frameUrl: string
   loop?: boolean
   playing?: boolean
   startFrame?: number
   styleFunction?: (frameNumber: number) => any
-  totalFrames: number
-}
-
-function frameStack(count: number) {
-  return new Array(count).fill(null)
 }
 
 function FlipBook({
+  frames,
+
   endFrame,
   flickerMax = 1,
   flickerMin = 0,
   flickerThreshold = 1,
   frameRate = 6, // TODO: Make this truly fps not some weird fudge factor
-  frameType = 'jpg',
-  frameUrl,
   loop = true,
   playing = true,
   startFrame = 0,
   styleFunction = () => {},
-  totalFrames,
 }: IProps): ReactElement {
-  const loopEnd = endFrame || totalFrames
+  const loopEnd = endFrame || frames.length - 1
 
   const [frameNumber, setFrameNumber] = useState(startFrame)
 
@@ -53,25 +47,25 @@ function FlipBook({
   }, [frameNumber, playing])
 
   const opacity = typeof flickerThreshold === 'number' ?
-    Math.random() < flickerThreshold ? flickerMax : flickerMin
-    : flickerThreshold(frameNumber)
+    Math.random() < flickerThreshold ?
+      flickerMax
+    : flickerMin
+      : flickerThreshold(frameNumber)
 
   return (
     <div
-      className="flipbook"
-      style={{
-        position: 'relative',
-      }}
+      className="flip-book"
+      style={{ position: 'relative' }}
     >
-      {frameStack(129).map((_, i) => (
+      {frames.map((src, myFrameNumber) => (
         <img
-          key={i}
-          src={`${frameUrl}/${i}.${frameType}`}
+          key={src}
+          src={src}
           style={{
             position: 'absolute',
             top: 0,
             left: 0,
-            opacity,
+            opacity: frameNumber === myFrameNumber ? opacity : 0,
             ...styleFunction(frameNumber)
           }}
         />
