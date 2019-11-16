6937620000d6247a60952780a76c494579ebe600
Jason Grier <jason@hemberlin.de>
2019-11-16 15:08:42 -0600

    Studio: Add demo movies page

--- src/projects/sites/studio/functions/generate-image-sequence-urls.ts
+++ src/projects/sites/studio/functions/generate-image-sequence-urls.ts
@@ -1,13 +1,6 @@
-interface IArguments {
-  base: string
-  end: number
-  ext: string
-  start: number
-}
-
-function generateImageSequenceUrls({base, start, end, ext}: IArguments) {
+function generateImageSequenceUrls(base, end, ext, start) {
   const images = []
-  for (let i = 3130; i <= 3255; i ++) { // TODO: Install Prettier/Eslint
+  for (let i = start; i <= end; i ++) { // TODO: Install Prettier/Eslint
     images.push(`${base}${i}.${ext}`)
   }
   return images
