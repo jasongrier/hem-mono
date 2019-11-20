793eb77cacb467efe364b43ca6c397038d1210a9
Jason Grier <jason@hemberlin.de>
2019-11-20 16:34:31 -0600

    Journal: Soft launch release

--- src/projects/sites/midst-journal/components/ProcessNote.tsx
+++ src/projects/sites/midst-journal/components/ProcessNote.tsx
@@ -63,12 +63,12 @@ function ProcessNote({ match }: IProps): ReactElement {
       >
         x
       </div>
-
-
-      <div className="process-note__content">
-        <div dangerouslySetInnerHTML={{__html: poem ? poem.processNote : ''}} />
-        <img src={`http://midst.press/static-assets/journal-assets/staging-authors/${poem.authorId}/${poem.authorId}.jpg`} />
-      </div>
+      {poem &&
+        <div className="process-note__content">
+          <div dangerouslySetInnerHTML={{__html: poem ? poem.processNote : ''}} />
+          <img src={`http://midst.press/static-assets/journal-assets/staging-authors/${poem.authorId}/${poem.authorId}.jpg`} />
+        </div>
+      }
     </div>
   )
 }
