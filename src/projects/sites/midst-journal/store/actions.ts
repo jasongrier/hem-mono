ce5af58b1fd423014fa4510d0e8add03b51a56a2
Jason Grier <jason@hemberlin.de>
2019-11-19 20:58:55 -0600

    Journal: On-demand loading

--- src/projects/sites/midst-journal/store/actions.ts
+++ src/projects/sites/midst-journal/store/actions.ts
@@ -13,49 +13,40 @@ import {
   IPoem,
 } from './types'
 
-const loadPoemData = (firstPoemIndex: number): any => // TODO: Should be: ThunkResult<void>. Why doesn't it work?
+const loadPoemData = (poemIndex: number): any => // TODO: Should be: ThunkResult<void>. Why doesn't it work?
   async (dispatch: any, getState: any) => { // TODO: Above should be: ThunkResult<void>, then `any` is not needed here
     let poems = [].concat(getState().app.poems)
-    poems = arrayMove(poems, firstPoemIndex, 0)
 
     const zip = (window as any).zip
     zip.workerScriptsPath = '/static-assets/website-assets/scripts/'
 
-    for (let i = 0; i < poems.length; i++) {
-      try {
-        // TODO: Gzip these on the server
-        const poem: IPoem = poems[i]
-        // TODO: Remove jQuery dependency
-        // TODO: This needs to be non-blocking!!!
-        // const data = await $.getJSON(`http://midst.press/static-assets/journal-assets/staging-authors/${poem.authorId}/${poem.poemId}.midst`)
-        // dispatch({ type: LOAD_POEM_DATA, payload: { poemId: poem.poemId, data, processNote }})
+    try {
+      const poem: IPoem = poems[poemIndex]
+      const processNoteRaw = await $.get(`http://midst.press/static-assets/journal-assets/staging-authors/${poem.authorId}/${poem.authorId}.md`)
+      const processNote = insane(marked(processNoteRaw), { allowedTags: ['h1', 'p', 'i', 'a', 'em', 'b', 'strong', 'img']})
+      const zipTest = await fetch(`http://midst.press/static-assets/journal-assets/staging-authors/${poem.authorId}/${poem.poemId}.midst.zip`)
+      const reader = new zip.BlobReader(await zipTest.blob())
 
-        const processNoteRaw = await $.get(`http://midst.press/static-assets/journal-assets/staging-authors/${poem.authorId}/${poem.authorId}.md`)
-        const processNote = insane(marked(processNoteRaw), { allowedTags: ['h1', 'p', 'i', 'a', 'em', 'b', 'strong', 'img']})
-        const zipTest = await fetch(`http://midst.press/static-assets/journal-assets/staging-authors/${poem.authorId}/${poem.poemId}.midst.zip`)
-        const reader = new zip.BlobReader(await zipTest.blob())
-
-        zip.createReader(reader, zipReader => {
-          zipReader.getEntries(entries => {
-            const writer = new zip.BlobWriter()
-            entries[0].getData(writer, blob => {
-              const reader = new FileReader()
-              reader.addEventListener('loadend', (e: any) => {
-                console.log(poem.poemId)
-                const data = JSON.parse(e.srcElement.result)
-                dispatch({ type: LOAD_POEM_DATA, payload: { poemId: poem.poemId, data, processNote }})
-              })
-              reader.readAsText(blob)
+      zip.createReader(reader, zipReader => {
+        zipReader.getEntries(entries => {
+          const writer = new zip.BlobWriter()
+          entries[0].getData(writer, blob => {
+            const reader = new FileReader()
+            reader.addEventListener('loadend', (e: any) => {
+              console.log(poem.poemId)
+              const data = JSON.parse(e.srcElement.result)
+              dispatch({ type: LOAD_POEM_DATA, payload: { poemId: poem.poemId, data, processNote }})
             })
+            reader.readAsText(blob)
           })
-        }, err => {
-          console.log(err)
         })
-      }
-
-      catch(err) {
+      }, err => {
         console.log(err)
-      }
+      })
+    }
+
+    catch(err) {
+      console.log(err)
     }
   }
 
