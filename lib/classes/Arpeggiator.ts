ea70d00ba36a2ebc3f960f07f2ceab589d9a832c
Jason Grier <jason@hemberlin.de>
2019-11-22 21:05:51 -0600

    Studio: Arpeggiator class WIP

---
+++ lib/classes/Arpeggiator.ts
@@ -0,0 +1,94 @@
+type Modes = 'across' | 'down' | 'random' | 'up'
+
+interface IDimensions {
+  x: number
+  y: number
+}
+
+interface IOpts {
+  dimensions: IDimensions
+}
+
+class Arpeggiator {
+  private dimensions: IDimensions
+  private mode: Modes
+  private notes: boolean[]
+  private numNotes: number
+  private position: number
+
+  constructor({ dimensions }: IOpts) {
+    this.dimensions = dimensions
+    this.mode = 'random'
+    this.numNotes = this.dimensions.x * this.dimensions.y
+    this.notes = this.fillNotes(this.numNotes)
+    this.position = 0
+  }
+
+  public activateNote(note: number) {
+    this.notes[note] = true
+  }
+
+  public deactivateNote(note: number) {
+    this.notes[note] = false
+  }
+
+  public setMode(mode: Modes) {
+    if (mode === this.mode) return
+
+    if (
+      this.mode === 'across'
+      || mode === 'across'
+    ) {
+      this.position = 0
+    }
+
+    this.mode = mode
+  }
+
+  public getNotes() {
+    if (this.mode === 'across') {
+      return this.pickAcross()
+    }
+
+    else if (this.mode === 'down') {
+      return this.pickDown()
+    }
+
+    else if (this.mode === 'random') {
+      return this.pickRandom()
+    }
+
+    else if (this.mode === 'up') {
+      return this.pickUp()
+    }
+  }
+
+  private fillNotes(numNotes: number): boolean[] {
+    const notes: boolean[] = []
+
+    for (let i = 0; i < numNotes; i ++) {
+      notes.push(false)
+    }
+
+    return notes
+  }
+
+  private pickRandom() {
+    this.position = Math.floor(Math.random() * this.notes.length - 1)
+    return this.position
+  }
+
+  private pickUp() {
+
+  }
+
+  private pickDown() {
+
+  }
+
+  private pickAcross() {
+
+  }
+}
+
+export default Arpeggiator
