af209743acab68a78810e5f5bbf648a40892c543
Jason Grier <jason@hemberlin.de>
2019-11-28 21:59:48 -0600

    SL2 Page: Add notes re: paywall, video wallpaper

--- projects/hem-rocks/routes/Sl.tsx
+++ projects/hem-rocks/routes/Sl.tsx
@@ -2,13 +2,16 @@ import React, { ReactElement } from 'react'
 import { useDispatch } from 'react-redux'
 import { Helmet } from 'react-helmet'
 
-function setHeroinePosition(position: number) {
-  // This should be a Thunk
+function setCarouselPosition(id: string, position: number) { // Redux Thunk Action
   // Trigger Slick carousel to the selected panel
   // Load global sound player with the preview sound #1 for this panel
 }
 
-function openSoundDownloadPopup(id: string, stack: boolean = false) {
+function advanceCarousel() { // Redux Thunk Action
+  // Same as above...
+}
+
+function openSoundDownloadPopup(id: string, stack: boolean = false) { // Redux Action
   // TODO: Modal module
 }
 
@@ -35,10 +38,10 @@ function Sl(): ReactElement {
           </header>
           <nav className="new-packs-carousel-nav">
             <ul>
-              <li onClick={() => dispatch(setHeroinePosition(1))}>Grand Piano</li>
-              <li onClick={() => dispatch(setHeroinePosition(3))}>Viola</li>
-              <li onClick={() => dispatch(setHeroinePosition(4))}>Noise Reduction Artefacts</li>
-              <li onClick={() => dispatch(setHeroinePosition(5))}>Seurat for Push</li>
+              <li onClick={() => dispatch(setCarouselPosition('new-packs-heroine', 1))}>Grand Piano</li>
+              <li onClick={() => dispatch(setCarouselPosition('new-packs-heroine', 3))}>Viola</li>
+              <li onClick={() => dispatch(setCarouselPosition('new-packs-heroine', 4))}>Noise Reduction Artefacts</li>
+              <li onClick={() => dispatch(setCarouselPosition('new-packs-heroine', 5))}>Seurat for Push</li>
             </ul>
           </nav>
           <div className="new-packs">
@@ -50,14 +53,18 @@ function Sl(): ReactElement {
                   {/* TODO: Closeup of the top four dials */}
                   {/* <img src="" alt=""/> */}
                 </div>
-                <div className="new-pack-playlist">
-                  <ul>
-                    <li>Jeux</li>
-                    <li>Pavan</li>
-                    <li>Gibet</li>
-                    <li>Lieb</li>
-                    <li>Fantas</li>
-                  </ul>
+                <div className="new-pack-movie-wallpaper">
+                  {/* TODO: This should be a flip book film and/or 3D CSS animation */}
+                </div>
+                <div className="new-pack-play">
+                  {/* TODO:
+                    Starts as a speaker icon, waveform playing
+                    First click turns up the volume and resets the track to zero
+                    Next button skips to the next demo, without seeming to advance the carousel
+                  */}
+                  <div className="new-pack-speaker-up-button"></div>
+                  <div className="new-pack-next-button"></div>
+                  <div className="new-pack-waveform"></div>
                 </div>
                 {/* TODO: Analytics event */}
                 <button
@@ -65,9 +72,14 @@ function Sl(): ReactElement {
                   onClick={() => dispatch(openSoundDownloadPopup('grand'))}
                 >
                   Download
+                  {/* TODO: Popup with suggested price paywall and not-too-hidden "I can't pay" link */}
                 </button>
               </div>
             {/* </Carousel> */}
+            <div
+              onClick={() => dispatch(advanceCarousel())}
+              className="new-packs-big-next-arrow"
+            />
           </div>
         {/* </Displace> */}
       </main>
