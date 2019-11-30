8d32f0b7a32bb64674371cc1e5cd0eff29e69d25
Jason Grier <jason@hemberlin.de>
2019-11-30 16:10:01 -0600

    HEM: Redux stuff for sample pack preview audio player

--- projects/hem-rocks/store/actions.ts
+++ projects/hem-rocks/store/actions.ts
@@ -1,13 +1,58 @@
-import { AnyAction } from 'redux'
 import {
-  SOME_ACTION
+  PLAYER_PLAY,
+  PLAYER_PAUSE,
+  PLAYER_SET_SOUND_URL,
+  PLAYER_SET_VOLUME,
+
+  ThunkResult,
 } from './types'
 
-const someAction = (): AnyAction => ({
-  type: SOME_ACTION,
-  payload: null,
-})
+const mockPlayer = {
+  load: async (url: string) => {},
+  pause: async () => {},
+  play: async () => {},
+  setVolume: async (volume: number) => {},
+}
+
+const playerPause = (): ThunkResult<void> =>
+  async (dispatch) => {
+    await mockPlayer.pause()
+    dispatch({ type: PLAYER_PAUSE, payload: null })
+  }
+
+const playerPlay = (): ThunkResult<void> =>
+  async (dispatch) => {
+    await mockPlayer.play()
+    dispatch({ type: PLAYER_PLAY, payload: null })
+  }
+
+const playerSetSoundUrl = (url: string): ThunkResult<void> =>
+  async (dispatch, getState) => {
+    const { playerPlaying } = (getState() as any).app
+    const playerWasPlaying = playerPlaying
+
+    if (playerPlaying) {
+      dispatch({ type: PLAYER_PAUSE, payload: null })
+    }
+
+    await mockPlayer.load(url)
+
+    dispatch({ type: PLAYER_SET_SOUND_URL, payload: url })
+
+    if (playerWasPlaying) {
+      dispatch({ type: PLAYER_PLAY, payload: null })
+    }
+  }
+
+const playerSetVolume = (volume: number): ThunkResult<void> => // TODO: Should be ThunkResult<IActionType>, everywhere
+  async (dispatch) => {
+    await mockPlayer.setVolume(volume)
+    dispatch({ type: PLAYER_SET_VOLUME, payload: volume })
+  }
 
 export {
-  someAction,
+  playerPause,
+  playerPlay,
+  playerSetSoundUrl,
+  playerSetVolume,
 }
--- projects/hem-rocks/store/types.ts
+++ projects/hem-rocks/store/types.ts
@@ -1,25 +1,42 @@
 import { AnyAction } from 'redux'
+import { ThunkAction } from 'redux-thunk'
 
-export interface ISlSoundPreset {
-  id: string
-  title: string
-}
-
-export interface ISlSound {
-  id: string
-  presets: ISlSoundPreset[]
-  title: string
-}
+// TODO: How to get around putting this in every project??
+export type ThunkResult<R> = ThunkAction<R, IState, undefined, Action>
 
 export interface IState {
-  slSounds: ISlSound[]
+  playerPlaying: boolean
+  playerVolume: number
+  playerSoundUrl: string
 }
 
-export const SOME_ACTION = 'SOME_ACTION'
+export const PLAYER_PLAY = 'PLAYER_PLAY'
+export const PLAYER_PAUSE = 'PLAYER_PAUSE'
+export const PLAYER_SET_SOUND_URL = 'PLAYER_SET_SOUND_URL'
+export const PLAYER_SET_VOLUME = 'PLAYER_SET_VOLUME'
 
-export interface ISomeAction extends AnyAction {
-  type: typeof SOME_ACTION
+export interface IPlayerPlay extends AnyAction {
+  type: typeof PLAYER_PLAY
   payload: null
 }
 
-export type Action = ISomeAction
+export interface IPlayerPause extends AnyAction {
+  type: typeof PLAYER_PAUSE
+  payload: null
+}
+
+export interface IPlayerSetSoundUrl extends AnyAction {
+  type: typeof PLAYER_SET_SOUND_URL
+  payload: string
+}
+
+export interface IPlayerSetVolume extends AnyAction {
+  type: typeof PLAYER_SET_VOLUME
+  payload: number
+}
+
+export type Action =
+  IPlayerPlay
+  | IPlayerPause
+  | IPlayerSetSoundUrl
+  | IPlayerSetVolume
