ef88460873c01d6e18ec492f41bd1d326d7c50ca
Annelyse Gelman <annelyse.robin@gmail.com>
2019-12-02 21:06:46 -0600

    Getting the TOC and letter to the editor ready

--- projects/midst-press/routes/Read.tsx
+++ projects/midst-press/routes/Read.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { Helmet } from 'react-helmet'
+import { ChevronButton } from '../../../lib/components/buttons'
 import { BASE_SITE_PAGE_TITLE } from '../config'
 import { RootState } from '../store'
 import { IPoem } from '../store/types'
@@ -24,6 +25,33 @@ function Read(): ReactElement {
       <section className="heroine">
         <div className="issue-date">Winter&nbsp;2019</div>
         <div className="issue-number">The Pilot Issue</div>
+        <div className="editor-letter-toggle">Letter from the Editor</div>
+        <div className="editor-letter">
+          <div className="editor-button">
+            <ChevronButton
+              onClick={() => {}}
+            />
+          </div>
+              <p> Dear friends,</p>
+              <p> 
+              Welcome to the debut issue of Midst! I've been fascinated with this concept—a journal that would let you see exactly how each of its poems were written—for years, and am so excited for you to finally see the first examples of brand-new works-in-process from these extraordinarily brave poets.
+              </p> 
+              <p> 
+              Inside, you'll find prose poetry, hyper-sparse forms, imitations, poetry in Korean (with both its initial process <i>and</i> the process of its translation), and more. Some of these poems 
+              are remarkably straightforward in their development, seeming to spill out all at once, intact; others 
+              evolve through long, winding strings of text that beautifully showcase the interplay of "writing" and "editing".
+              </p>
+              <p> 
+              Future issues will have even more great features, and I welcome your comments and suggestions—feel free to reach out to midsthq@gmail.com. You can subscribe to our newsletter below and follow us at <a href="http://www.twitter.com/midstpoetry">@midstpoetry</a>.
+              </p> 
+              <p> 
+              Happy reading!
+              </p> 
+              <p> 
+                Annelyse
+              </p> 
+        </div>
+
         {poems.map((poem: IPoem) => (
           <Link key={poem.url}
             className="poem-link"
