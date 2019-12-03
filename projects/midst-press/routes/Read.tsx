d3784890f75468743696d5a1fda835e428ed07ab
Jason Grier <jason@hemberlin.de>
2019-12-02 21:50:29 -0600

    Journal: Open/close editor note

--- projects/midst-press/routes/Read.tsx
+++ projects/midst-press/routes/Read.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { Helmet } from 'react-helmet'
@@ -15,6 +15,26 @@ function Read(): ReactElement {
 
   const dispatch = useDispatch()
 
+  const [editorLetterOpen, setEditorLetterOpen] = useState(false)
+
+  useEffect(() => {
+    // TODO: React "call on an unmounted component" warning
+    // 1) Open/close the editor note
+    // 2) Go to a poem and open/close the process note
+    // 3) Go home
+    // 4) Go back here and open/close the editor note, using ESC
+    document.body.addEventListener('keydown', closeEditorLetter)
+    return function cleanup() {
+      document.body.removeEventListener('keydown', closeEditorLetter)
+    }
+  }, [])
+
+  function closeEditorLetter(evt: any) {
+    if (evt.keyCode === 27) {
+      setEditorLetterOpen(false)
+    }
+  }
+
   return (
     <div className="table-of-contents">
       <Helmet>
@@ -25,31 +45,41 @@ function Read(): ReactElement {
       <section className="heroine">
         <div className="issue-date">Winter&nbsp;2019</div>
         <div className="issue-number">The Pilot Issue</div>
-        <div className="editor-letter-toggle">Letter from the Editor</div>
-        <div className="editor-letter">
-          <div className="editor-button">
-            <ChevronButton
-              onClick={() => {}}
-            />
+        <div
+          className="editor-letter-toggle"
+          onClick={() => setEditorLetterOpen(!editorLetterOpen)}
+        >
+          Letter from the Editor
+        </div>
+        <div className="editor-letter-container">
+          <div className={`
+            editor-letter
+            ${editorLetterOpen ? 'editor-letter-open' : ''}
+          `}>
+            <div className="editor-button">
+              <ChevronButton
+                onClick={() => setEditorLetterOpen(!editorLetterOpen)}
+              />
+            </div>
+            <p> Dear friends,</p>
+            <p>
+            Welcome to the debut issue of Midst! I've been fascinated with this concept—a journal that would let you see exactly how each of its poems were written—for years, and am so excited for you to finally see the first examples of brand-new works-in-process from these extraordinarily brave poets.
+            </p>
+            <p>
+            Inside, you'll find prose poetry, hyper-sparse forms, imitations, poetry in Korean (with both its initial process <i>and</i> the process of its translation), and more. Some of these poems
+            are remarkably straightforward in their development, seeming to spill out all at once, intact; others
+            evolve through long, winding strings of text that beautifully showcase the interplay of "writing" and "editing".
+            </p>
+            <p>
+            Future issues will have even more great features, and I welcome your comments and suggestions—feel free to reach out to midsthq@gmail.com. You can subscribe to our newsletter below and follow us at <a href="http://www.twitter.com/midstpoetry">@midstpoetry</a>.
+            </p>
+            <p>
+            Happy reading!
+            </p>
+            <p>
+              Annelyse
+            </p>
           </div>
-              <p> Dear friends,</p>
-              <p> 
-              Welcome to the debut issue of Midst! I've been fascinated with this concept—a journal that would let you see exactly how each of its poems were written—for years, and am so excited for you to finally see the first examples of brand-new works-in-process from these extraordinarily brave poets.
-              </p> 
-              <p> 
-              Inside, you'll find prose poetry, hyper-sparse forms, imitations, poetry in Korean (with both its initial process <i>and</i> the process of its translation), and more. Some of these poems 
-              are remarkably straightforward in their development, seeming to spill out all at once, intact; others 
-              evolve through long, winding strings of text that beautifully showcase the interplay of "writing" and "editing".
-              </p>
-              <p> 
-              Future issues will have even more great features, and I welcome your comments and suggestions—feel free to reach out to midsthq@gmail.com. You can subscribe to our newsletter below and follow us at <a href="http://www.twitter.com/midstpoetry">@midstpoetry</a>.
-              </p> 
-              <p> 
-              Happy reading!
-              </p> 
-              <p> 
-                Annelyse
-              </p> 
         </div>
 
         {poems.map((poem: IPoem) => (
