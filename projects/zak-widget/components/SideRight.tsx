db2ea5866290066eb6fc84ca251b172929937f27
Jason Grier <jason@hemberlin.de>
2019-12-14 15:41:17 -0600

    Get tint from a product

--- projects/zak-widget/components/SideRight.tsx
+++ projects/zak-widget/components/SideRight.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useCallback, useEffect } from 'react'
+import React, { ReactElement, useCallback } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../store'
 import OptionRow from './OptionRow'
