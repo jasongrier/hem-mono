e78620b1a3c1ee56a5e3251b3f473ba6e1c6c2ca
Jason Grier <jason@hemberlin.de>
2019-12-14 10:57:36 -0600

    Zak: Put hardcoded options in index.html

--- projects/zak-widget/functions/get-product-options/get-high-index-option.ts
+++ projects/zak-widget/functions/get-product-options/get-high-index-option.ts
@@ -1,7 +1,7 @@
-declare const HIGH_INDEX_ADD_ON_PRODUCT: string
+declare const PDP_WIDGET_HIGH_INDEX_ADD_ON_PRODUCT: string
 
 function getHighIndexOption() {
-  const highIndexOption = JSON.parse(HIGH_INDEX_ADD_ON_PRODUCT)
+  const highIndexOption = JSON.parse(PDP_WIDGET_HIGH_INDEX_ADD_ON_PRODUCT)
   return {
     title: highIndexOption.title,
     price: highIndexOption.price / 100,
