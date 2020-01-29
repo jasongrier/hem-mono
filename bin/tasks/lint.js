c209948d645c12d8c9fdee7208a98197a9c7afc1
Jason Grier <jason@hemberlin.de>
2020-01-29 17:07:20 -0600

    WIP

--- bin/tasks/lint.js
+++ bin/tasks/lint.js
@@ -1,20 +1,19 @@
 const { join } = require('path')
-const { readdirSync } = require('fs')
+const { readdirSync, lstatSync } = require('fs')
 const doAll = require('../helpers/do-all')
 
 const expectedProjectFiles = [
-  'classes',
   'components',
   'functions',
   'hooks',
+  'modules',
   'routes',
   'static',
-  'store',
+  'styles',
   'tests',
 
   '.htaccess',
   'config.ts',
-  'index.css',
   'index.html',
   'index.ts',
   'README.md',
@@ -23,23 +22,19 @@ const expectedProjectFiles = [
 ]
 
 const expectedComponentsFiles = [
-  'actions.ts',
+  'App.tsx',
   'index.ts',
-  'reducer.ts',
-  'selectors.ts',
-  'types.ts',
+  'Root.tsx'
 ]
 
-const expectedRoutesFiles = [
-  'Home.tsx',
+const expectedFunctionsFiles = [
   'index.ts',
 ]
 
-const expectedStoreFiles = [
+const expectedModulesFiles = [
   'actions.ts',
   'index.ts',
   'reducer.ts',
-  'sagas.ts',
   'selectors.ts',
   'types.ts',
 ]
@@ -62,16 +57,34 @@ const ignoredFiles = [
   '.DS_Store',
 ]
 
-let expectedFilesFound = []
-let ignoredFilesFound = []
-let unexpectedFilesFound = []
+function lintProject(projectName) {
+  const projectDir = join(__dirname, '..', '..', 'projects', projectName)
 
-function lintFiles(dir, expectedFiles) {
-  expectedFilesFound = []
-  ignoredFilesFound = []
-  unexpectedFilesFound = []
+  lintFiles(projectName, projectDir, expectedProjectFiles)
 
-  readdirSync(dir).forEach(filePath => {
+  // lintFiles(join(projectDir, 'components'), expectedComponentsFiles)
+  // evaluateLint(projectName, expectedModuleFiles, 'components/')
+
+  // lintFiles(join(projectDir, 'routes'), expectedModuleFiles)
+  // evaluateLint(projectName, expectedModuleFiles, 'routes/', false)
+
+  // lintFiles(join(projectDir, 'static'), expectedStaticFiles)
+  // evaluateLint(projectName, expectedStaticFiles, 'static/')
+
+  // lintFiles(join(projectDir, 'store'), expectedStoreFiles)
+  // evaluateLint(projectName, expectedStoreFiles, 'store/')
+
+  // lintFiles(join(projectDir, 'tests'), expectedTestsFiles)
+  // // TODO: This bypasses checks for, for example, `i-dont-belong.foo`, the correct check should be `requiredFiles`
+  // evaluateLint(projectName, expectedTestsFiles, 'tests/', false)
+}
+
+function lintFiles(projectName, checkDir, expectedFiles) {
+  const expectedFilesFound = []
+  const ignoredFilesFound = []
+  const unexpectedFilesFound = []
+
+  readdirSync(checkDir).forEach(filePath => {
     if (expectedFiles.indexOf(filePath) > -1) {
       expectedFilesFound.push(filePath)
     }
@@ -84,54 +97,20 @@ function lintFiles(dir, expectedFiles) {
       unexpectedFilesFound.push(filePath)
     }
   })
-}
 
-function lintProject(projectName) {
-  const projectDir = join(__dirname, '..', '..', 'projects', projectName)
-
-  lintFiles(projectDir, expectedProjectFiles)
-  const skip = evaluateLint(projectName, expectedProjectFiles)
-
-  if (skip) return
-
-  lintFiles(join(projectDir, 'store'), expectedComponentsFiles)
-  evaluateLint(projectName, expectedComponentsFiles, 'components/')
-
-  lintFiles(join(projectDir, 'routes'), expectedRoutesFiles)
-  evaluateLint(projectName, expectedRoutesFiles, 'routes/', false)
-
-  lintFiles(join(projectDir, 'static'), expectedStaticFiles)
-  evaluateLint(projectName, expectedStaticFiles, 'static/')
-
-  lintFiles(join(projectDir, 'store'), expectedStoreFiles)
-  evaluateLint(projectName, expectedStoreFiles, 'store/')
-
-  lintFiles(join(projectDir, 'tests'), expectedTestsFiles)
-  // TODO: This bypasses checks for, for example, `i-dont-belong.foo`, the correct check should be `requiredFiles`
-  evaluateLint(projectName, expectedTestsFiles, 'tests/', false)
-}
-
-function evaluateLint(projectName, expectedFiles, prefix = '', checkUnexpected = true) {
   if (expectedFiles.length !== expectedFilesFound.length) {
     expectedFiles.forEach(filePath => {
       if (expectedFilesFound.indexOf(filePath) === -1) {
-        console.log(`!!!! Sorry ${projectName}, seems like you are missing ${prefix}${filePath}`)
+        console.log(`!!!! Sorry ${projectName}, seems like you are missing ${filePath}`)
       }
     })
   }
 
-  if (checkUnexpected && unexpectedFilesFound.length) {
+  if (unexpectedFilesFound.length) {
     unexpectedFilesFound.forEach(filePath => {
-      console.log(`!!!! Sorry ${projectName}, seems like you have ${prefix}${filePath} where it does not belong`)
+      console.log(`!!!! Sorry ${projectName}, seems like you have ${filePath} where it does not belong`)
     })
   }
-
-  // if ( // TODO: Verbose flag for this
-  //   expectedFiles.length === expectedFilesFound.length
-  //   && !unexpectedFilesFound.length
-  // ) {
-  //   console.log(`${projectName}${prefix ? '/' + prefix : ''} –– OK`)
-  // }
 }
 
 function lint(projectName) {
--- projects/jasonaarongrier-com/modules/articles/actions.ts
+++ projects/jasonaarongrier-com/modules/articles/actions.ts
@@ -8,7 +8,7 @@ import {
 const loadItems = (): ThunkResult<void> =>
   async (dispatch) => {
     const articlesList = await fetch('/static/data/index.json')
-    dispatch({ type: LOAD_ITEMS, payload: 'b' })
+    dispatch({ type: LOAD_ITEMS, payload: null })
   }
 
 export {
