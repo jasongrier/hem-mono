c2147caad7e80abdcc448dec091f181274301f94
Jason Grier <jason@hemberlin.de>
2020-01-31 17:14:45 -0800

    Zak: Collections widget final

--- projects/zak-collections-widget/components/App.tsx
+++ projects/zak-collections-widget/components/App.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react'
 import { reduce } from 'lodash'
+import { trim } from 'voca'
+import romanNumeralToDecimal from 'roman-numeral-to-decimal'
 import ItemRow from './ItemRow'
 
 declare const COLLECTIONS_WIDGET_PRODUCTS: string
@@ -13,10 +15,16 @@ const options = reduce(PDP_WIDGET_PRODUCT_OPTIONS_WITH_VALUES, (acc, optsString,
   return acc
 }, {} as any)
 
+const itemsSorted = items.sort((a, b) => {
+  const numeralA = romanNumeralToDecimal(trim(a.title.replace(collection.title, '')))
+  const numeralB = romanNumeralToDecimal(trim(b.title.replace(collection.title, '')))
+  return numeralB - numeralA
+})
+
 function App(): ReactElement {
   return (
     <div className="hem-application">
-      {items.reverse().map((item: any) => (
+      {itemsSorted.map((item: any) => (
         <ItemRow
           collection={collection}
           item={item}
