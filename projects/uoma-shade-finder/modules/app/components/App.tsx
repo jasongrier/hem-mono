65b3af6d817527042ef1de5d289b10b680b6aec8
Jason Grier <jason@hemberlin.de>
2020-02-10 20:03:55 +0100

    Uoma: Step six deleted

--- projects/uoma-shade-finder/modules/app/components/App.tsx
+++ projects/uoma-shade-finder/modules/app/components/App.tsx
@@ -8,7 +8,6 @@ import Step2 from './Step2'
 import Step3 from './Step3'
 import Step4 from './Step4'
 import Step5 from './Step5'
-import Step6 from './Step6'
 import StepsProgress from './StepsProgress'
 
 function App(): ReactElement {
@@ -47,7 +46,6 @@ function App(): ReactElement {
           <Step3 />
           <Step4 />
           <Step5 />
-          <Step6 />
         </div>
 
         { currentStep > 1 &&
