9cf879793bdb6c82c0de6c89148f68c1d2a1f5f4
Emily Kuntz <emily.jane.kuntz@gmail.com>
2020-02-17 09:40:30 -0800

    Design tweaks etc

--- projects/uoma-shade-finder/modules/app/components/Step5.tsx
+++ projects/uoma-shade-finder/modules/app/components/Step5.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../../index'
+import $ from "jquery";
 import ProductTile from './ProductTile'
 
 function Step5(): ReactElement {
@@ -33,12 +34,13 @@ function Step5(): ReactElement {
   return (
     <div className="step-content step-5">
       <h1>You are...</h1>
+      <h1 className="solution-banner">Brown Sugar Brown Sugar Brown Sugar Brown Sugar Brown Sugar Brown Sugar Brown Sugar Brown Sugar Brown Sugar Brown Sugar</h1>
       <div className="solution-layout">
-        <div className="solution-layout-row">
+        <div className="solution-layout-row solution-hero-container">
           <div className="solution-product solution-product-foundation">
             <ProductTile
               ctaOnClick={() => {}}
-              ctaText="Shop now"
+              ctaText="Add to bag"
               imgSrc="http://placekitten.com/1000/1000"
               price="$39.00 USD"
               reviewsCount="43"
@@ -48,8 +50,7 @@ function Step5(): ReactElement {
             />
           </div>
           <div className="solution-hero">
-            <div className="solution-hero-pic" >
-              <img src="http://placekitten.com/900/900" alt=""/>
+            <div className="solution-hero-pic" style={{ backgroundImage: 'url(http://placekitten.com/900/1000)' }} >
             </div>
             <div className="solution-hero-text">
               <h2>Brown Sugar T2C</h2>
@@ -57,14 +58,19 @@ function Step5(): ReactElement {
               Lorem ipsum dolor sit amet.
               </h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+              <h2 className="solution-page-scroll">Shop Now</h2>
             </div>
           </div>
         </div>
+        <div className="solution-scroll-container">
+          <h3>Scroll for More</h3>
+          <span className="solution-chevron"></span>
+        </div>    
         <div className="solution-layout-row">
           <ProductTile
             ctaOnClick={() => {}}
             ctaText="Add to bag"
-            imgSrc="http://placekitten.com/1000/1000"
+            imgSrc="http://placekitten.com/2000/2000"
             price="$39.00 USD"
             reviewsCount="43"
             starsCount={4}
@@ -74,7 +80,7 @@ function Step5(): ReactElement {
           <ProductTile
             ctaOnClick={() => {}}
             ctaText="Add to bag"
-            imgSrc="http://placekitten.com/1000/1000"
+            imgSrc="http://placekitten.com/1001/1000"
             price="$39.00 USD"
             reviewsCount="43"
             starsCount={4}
@@ -84,7 +90,7 @@ function Step5(): ReactElement {
           <ProductTile
             ctaOnClick={() => {}}
             ctaText="Add to bag"
-            imgSrc="http://placekitten.com/1000/1000"
+            imgSrc="http://placekitten.com/899/900"
             price="$39.00 USD"
             reviewsCount="43"
             starsCount={4}
