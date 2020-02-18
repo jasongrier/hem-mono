24df81f0015cf17015877cc008c92a59cf9f2394
Jason Grier <jason@hemberlin.de>
2020-02-18 16:45:48 +0100

    Uoma: Finale product thumbnails

--- projects/uoma-shade-finder/modules/app/components/Step5.tsx
+++ projects/uoma-shade-finder/modules/app/components/Step5.tsx
@@ -1,7 +1,14 @@
 import React, { ReactElement } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { map, find } from 'lodash'
 import { RootState } from '../../../index'
 import ProductTile from './ProductTile'
+
+declare const toneMatrix: any
+declare var skinToneOptions: any[]
+declare const jsQuizResultsKey: any
+declare const jsQuizResultsById: any
+declare const allProducts: any
 
 function Step5(): ReactElement {
   const {
@@ -20,81 +27,108 @@ function Step5(): ReactElement {
     undertoneOption: state.app.undertoneOption,
   }))
 
-  console.log(currentStep)
-
   if (currentStep === 5) {
-    console.log('shadeOption', shadeOption)
-    console.log('skinToneOption', skinToneOption)
-    console.log('subQuizTone', subQuizTone)
-    console.log('subQuizVeins', subQuizVeins)
-    console.log('undertoneOption', undertoneOption)
-  }
+    const allVariants = allProducts.reduce((acc, product) => {
+      const variants = product.variants.map((variant) => {
+        variant.productId = product.id
+        return variant
+      })
 
-  return (
-    <div className="step-content step-5">
-      <h1>You are...</h1>
-      <div className="solution-layout">
-        <div className="solution-layout-row">
-          <div className="solution-product solution-product-foundation">
+      acc = acc.concat(variants)
+
+      return acc
+    }, [])
+
+    const shadeOptionNumber = parseInt(shadeOption) + 1
+    const solution = `${skinToneOption}-t${(shadeOptionNumber)}-${undertoneOption}`
+    const mainImage = skinToneOptions[skinToneOption][shadeOptionNumber]
+
+    const foundationVariantId = jsQuizResultsById[solution].foundation
+    const concealerVariantId = jsQuizResultsById[solution].concealer
+    const contourStickVariantId = parseInt(toneMatrix[skinToneOption].contour, 10)
+    const spongeProduct = find(allProducts, { handle: 'big-head-dual-density-makeup-sponge' })
+    const spongeVariant = spongeProduct.variants[0]
+
+    const foundationVariant = find(allVariants, { id: foundationVariantId })
+    const foundationProduct = find(allProducts, { id: foundationVariant.productId })
+    const concealerVariant = find(allVariants, { id: concealerVariantId })
+    const concealerProduct = find(allProducts, { id: concealerVariant.productId })
+
+    const contourStickVariant = find(allVariants, { id: contourStickVariantId })
+    const contourStickProduct = find(allProducts, { id: contourStickVariant.productId })
+
+    console.log(mainImage)
+
+    return (
+      <div className="step-content step-5">
+        <h1>You are...</h1>
+        <div className="solution-layout">
+          <div className="solution-layout-row">
+            <div className="solution-product solution-product-foundation">
+              <ProductTile
+                ctaOnClick={() => {}}
+                ctaText="Shop now"
+                imgSrc={foundationVariant.featured_image.src}
+                price={`$${foundationVariant.price / 100}.00`}
+                reviewsCount="43"
+                starsCount={4}
+                subtitle={`in ${foundationVariant.title}`}
+                title={foundationProduct.title}
+              />
+            </div>
+            <div className="solution-hero">
+              <div className="solution-hero-pic" >
+                <img src={mainImage} alt=""/>
+              </div>
+              <div className="solution-hero-text">
+                <h2>{ foundationVariant.title.replace(' - ', ' ') }</h2>
+                <h3>
+                  Lorem ipsum dolor sit amet.
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+              </div>
+            </div>
+          </div>
+          <div className="solution-layout-row">
             <ProductTile
               ctaOnClick={() => {}}
-              ctaText="Shop now"
-              imgSrc="http://placekitten.com/1000/1000"
-              price="$39.00 USD"
+              ctaText="Add to bag"
+              imgSrc={concealerVariant.featured_image.src}
+              price={`$${concealerVariant.price / 100}.00`}
               reviewsCount="43"
               starsCount={4}
-              subtitle="in Brown Sugar - T4W"
-              title="Say What?! Foundation"
+              subtitle={`in ${concealerVariant.title}`}
+              title={concealerProduct.title}
+            />
+            <ProductTile
+              ctaOnClick={() => {}}
+              ctaText="Add to bag"
+              imgSrc={contourStickVariant.featured_image.src}
+              price={`$${contourStickVariant.price / 100}.00`}
+              reviewsCount="43"
+              starsCount={4}
+              subtitle={`in ${contourStickVariant.title}`}
+              title={contourStickProduct.title}
+            />
+            <ProductTile
+              ctaOnClick={() => {}}
+              ctaText="Add to bag"
+              imgSrc={spongeVariant.featured_image.src}
+              price={`$${spongeVariant.price / 100}.00`}
+              reviewsCount="43"
+              starsCount={4}
+              subtitle={`in ${spongeVariant.title}`}
+              title={spongeProduct.title}
             />
           </div>
-          <div className="solution-hero">
-            <div className="solution-hero-pic" >
-              <img src="http://placekitten.com/900/900" alt=""/>
-            </div>
-            <div className="solution-hero-text">
-              <h2>Brown Sugar T2C</h2>
-              <h3>
-              Lorem ipsum dolor sit amet.
-              </h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-            </div>
-          </div>
-        </div>
-        <div className="solution-layout-row">
-          <ProductTile
-            ctaOnClick={() => {}}
-            ctaText="Add to bag"
-            imgSrc="http://placekitten.com/1000/1000"
-            price="$39.00 USD"
-            reviewsCount="43"
-            starsCount={4}
-            subtitle="in Brown Sugar - T4W"
-            title="Say What?! Foundation"
-          />
-          <ProductTile
-            ctaOnClick={() => {}}
-            ctaText="Add to bag"
-            imgSrc="http://placekitten.com/1000/1000"
-            price="$39.00 USD"
-            reviewsCount="43"
-            starsCount={4}
-            subtitle="in Brown Sugar - T4W"
-            title="Say What?! Foundation"
-          />
-          <ProductTile
-            ctaOnClick={() => {}}
-            ctaText="Add to bag"
-            imgSrc="http://placekitten.com/1000/1000"
-            price="$39.00 USD"
-            reviewsCount="43"
-            starsCount={4}
-            subtitle="in Brown Sugar - T4W"
-            title="Say What?! Foundation"
-          />
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  else {
+    return <div></div>
+  }
 }
 
 export default Step5
