7c044ff3abf552a426a36b965cc11d8d66bed1b6
Jason Grier <jason@hemberlin.de>
2020-04-02 08:02:47 +0200

    HEM: Micro updates

---
+++ lib/modules/player/components/PlayButton.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface IProps {
+
+}
+
+function PlayButton({ }: IProps): ReactElement {
+  return (
+    <button className="hem-player-play-button" />
+  )
+}
+
+export default PlayButton
