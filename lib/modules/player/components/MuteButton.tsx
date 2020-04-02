27a75d3a15c15aee29a2f5343b79bf3509615481
Jason Grier <jason@hemberlin.de>
2020-04-02 08:02:47 +0200

    HEM: Micro updates

---
+++ lib/modules/player/components/MuteButton.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface IProps {
+
+}
+
+function MuteButton({ }: IProps): ReactElement {
+  return (
+    <button className="hem-player-mute-button" />
+  )
+}
+
+export default MuteButton
