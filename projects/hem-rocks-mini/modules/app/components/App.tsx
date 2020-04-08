2440218cd2949df465590c5f4f52a94de6b40652
Jason Grier <jason@hemberlin.de>
2020-04-08 18:13:58 +0200

    Proper route structure

--- projects/hem-rocks-mini/modules/app/components/App.tsx
+++ projects/hem-rocks-mini/modules/app/components/App.tsx
@@ -2,24 +2,22 @@ import React, { ReactElement, useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import $ from 'jquery'
-import { debounce } from 'lodash'
-import { Home } from '../../../routes'
 import { BuyPopUp } from '../../products'
 import { PopupContainer } from '../../../../../lib/modules/popups'
+import { MainNavItem, TopBar } from '../../../components'
+import { GrandPianoHeroineAlternate } from '../../../components/heroines'
 import { RootState } from '../../../index'
 import { setTopBarCollapsed } from '../actions'
 
 function App(): ReactElement {
   const {
     activated,
-    currentTag,
     topBarCollapsed,
 
     currentlyOpenPopUp,
     popupData,
   } = useSelector((state: RootState) => ({
     activated: state.app.activated,
-    currentTag: state.app.currentTag,
     topBarCollapsed: state.app.topBarCollapsed,
 
     currentlyOpenPopUp: state.popups.currentlyOpenPopUp,
@@ -56,9 +54,83 @@ function App(): ReactElement {
       ${activated ? ' app-activated' : ''}
       ${topBarCollapsed ? ' top-bar-collapsed' : ''}
     `}>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
+      <TopBar collapsed={topBarCollapsed} />
+
+      <div className="main-heroine">
+        <GrandPianoHeroineAlternate />
+      </div>
+
+      <nav className="main-nav">
+        <ul>
+          <MainNavItem name="Sound Library" />
+          <MainNavItem name="Label" />
+          <MainNavItem name="Projects" />
+          <MainNavItem name="Info" />
+        </ul>
+      </nav>
+
+      <div className="pricing-banner" hidden>
+        Pay what you can. All devices run in Ableton Live Lite
+      </div>
+
+      <div className="sub-heroine new-devices-heroine" hidden>
+        <h2>New Devices</h2>
+        <div className="sub-heroine-columns">
+          <div className="sub-heroine-column">
+            {/* <ProductTile
+              title="Seurat 2"
+              featureList={[
+                'Supports multiple Seurat devices per track for more complex patterns',
+                'Generates both completely random and semi-random melodic patterns',
+                'Works entirely in Live Lite', // TODO: Place  repeated string in config
+              ]}
+              imgSrc=""
+              imgAlt=""
+              productId=""
+              videoPopUpId=""
+            /> */}
+          </div>
+          <div className="sub-heroine-column">
+            {/* <ProductTile
+              title="Chord Brush"
+              featureList={[
+                'Supports multiple Seurat devices per track for more complex patterns',
+                'Generates both completely random and semi-random melodic patterns',
+                'Works entirely in Live Lite', // TODO: Place  repeated string in config
+              ]}
+              imgSrc=""
+              imgAlt=""
+              productId=""
+              videoPopUpId=""
+            /> */}
+          </div>
+          <div className="sub-heroine-column">
+            {/* <ProductTile
+              title="Voice Splitter"
+              featureList={[
+                'Takes a single melody line and passes it around to different instruments',
+                'Can go note-by-note or according to various patterns',
+                'Works entirely in Live Lite', // TODO: Place  repeated string in config
+              ]}
+              imgSrc=""
+              imgAlt=""
+              productId=""
+              videoPopUpId=""
+            /> */}
+          </div>
+        </div>
+      </div>
+
+      <main className="main-content">
+        <div className="tabs-content">
+          <Switch>
+            <Route exact path="/sound-library" render={props => { return <h1>SOUND LIBRARY</h1>}}/>
+            <Route exact path="/label" render={props => { return <h1>LABEL</h1>}}/>
+            <Route exact path="/projects" render={props => { return <h1>PROJECTS</h1>}}/>
+            <Route exact path="/info" render={props => { return <h1>INFO</h1>}}/>
+          </Switch>
+        </div>
+      </main>
       <PopupContainer isOpen={currentlyOpenPopUp === 'buy-pop-up'}>
         <BuyPopUp product={popupData} />
       </PopupContainer>
