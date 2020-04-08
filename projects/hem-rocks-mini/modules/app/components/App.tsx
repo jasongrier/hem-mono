54c926297b86a417b4fe8b18f1a7b862a5f92bd2
Jason Grier <jason@hemberlin.de>
2020-04-08 17:52:12 +0200

    Finally got swing navigation right

--- projects/hem-rocks-mini/modules/app/components/App.tsx
+++ projects/hem-rocks-mini/modules/app/components/App.tsx
@@ -1,22 +1,60 @@
 import React, { ReactElement, useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import $ from 'jquery'
+import { debounce } from 'lodash'
 import { Home } from '../../../routes'
 import { BuyPopUp } from '../../products'
 import { PopupContainer } from '../../../../../lib/modules/popups'
 import { RootState } from '../../../index'
+import { setTopBarCollapsed } from '../actions'
 
 function App(): ReactElement {
-  const { activated, currentlyOpenPopUp, popupData } = useSelector((state: RootState) => ({
+  const {
+    activated,
+    currentTag,
+    topBarCollapsed,
+
+    currentlyOpenPopUp,
+    popupData,
+  } = useSelector((state: RootState) => ({
     activated: state.app.activated,
+    currentTag: state.app.currentTag,
+    topBarCollapsed: state.app.topBarCollapsed,
+
     currentlyOpenPopUp: state.popups.currentlyOpenPopUp,
     popupData: state.popups.popupData,
   }))
+
+  const dispatch = useDispatch()
+
+  useEffect(function checkOnInit() {
+    checkScrollTop()
+  }, [])
+
+  useEffect(function scrollSpy() {
+    $(window).on('scroll', checkScrollTop)
+  }, [topBarCollapsed])
+
+  function checkScrollTop() {
+    const scrollTop = $(window).scrollTop()
+
+    if (!scrollTop) return
+
+    if (scrollTop >= 665 && !topBarCollapsed) {
+      dispatch(setTopBarCollapsed(true))
+    }
+
+    else if (scrollTop < 665 && topBarCollapsed) {
+      dispatch(setTopBarCollapsed(false))
+    }
+  }
 
   return (
     <div className={`
       hem-application
       ${activated ? ' app-activated' : ''}
+      ${topBarCollapsed ? ' top-bar-collapsed' : ''}
     `}>
       <Switch>
         <Route path="/" component={Home} />
