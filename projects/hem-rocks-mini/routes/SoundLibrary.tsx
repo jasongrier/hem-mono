a26a09e402c925d20c21f8dce8eddebef3d3169f
Jason Grier <jason@hemberlin.de>
2020-04-08 18:13:58 +0200

    Proper route structure

---
+++ projects/hem-rocks-mini/routes/SoundLibrary.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+function SoundLibrary(): ReactElement {
+  return (
+    <div className="page page-sound-library">
+
+    </div>
+  )
+}
+
+export default SoundLibrary
