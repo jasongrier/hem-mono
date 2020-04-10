2d51d0dbccc8dbb0494c033f7942b2c4a2806ead
Jason Grier <jason@hemberlin.de>
2020-04-10 13:46:34 +0200

    Cart as far as possible without connecting to Shopify

--- projects/hem-rocks-mini/modules/products/components/CartPopup.tsx
+++ projects/hem-rocks-mini/modules/products/components/CartPopup.tsx
@@ -1,9 +1,10 @@
-import React, { ReactElement, useCallback } from 'react'
+import React, { ReactElement, useCallback, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { CloseButton } from '../../../../../lib/packages/hem-buttons'
 import { IProduct } from '../../products'
 import { RootState } from '../../../index'
 import { removeProductFromCart } from '../actions'
+import { closePopup } from '../../../../../lib/modules/popups'
 
 function CartPopup(): ReactElement {
   const { cartProducts } = useSelector((state: RootState) => ({
@@ -11,6 +12,12 @@ function CartPopup(): ReactElement {
   }))
 
   const dispatch = useDispatch()
+
+  const checkoutOnClick = useCallback(
+    function checkoutOnClickFn() {
+      // Submit to shopify cart and redirect
+    }, [],
+  )
 
   function getFinalPrice(product: IProduct) {
     let price
@@ -23,6 +30,26 @@ function CartPopup(): ReactElement {
       price = product.userSuggestedPrice
     }
 
+    return price
+  }
+
+  function getSubotal() {
+    // @ts-ignore
+    return cartProducts.reduce((acc: number, product: IProduct) => {
+      console.log(acc, getFinalPrice(product))
+      return acc + getFinalPrice(product)
+    }, 0)
+  }
+
+  function getTax() {
+    return 0
+  }
+
+  function getGrandTotal() {
+    return getSubotal() + getTax()
+  }
+
+  function formatPrice(price: number) {
     return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(price)
   }
 
@@ -38,22 +65,48 @@ function CartPopup(): ReactElement {
           </div>
         )}
         {!!cartProducts.length && (
-          <div className="cart-popup-items">
-            {cartProducts.map(product => (
-              <div className="cart-popup-item">
-                <div className="cart-popup-item-remove">
-                  <CloseButton
-                    onClick={() => {
-                      dispatch(removeProductFromCart(product.id))
-                    }}
-                  />
+          <>
+            <div className="cart-popup-items">
+              {cartProducts.map(product => (
+                <div
+                  className="cart-popup-item"
+                  key={product.id}
+                >
+                  <div className="cart-popup-item-remove">
+                    <CloseButton
+                      onClick={() => {
+                        dispatch(removeProductFromCart(product.id))
+                      }}
+                    />
+                  </div>
+                  <h2>{product.name}</h2>
+                  <p>{ product.type }</p>
+                  <div className="cart-popup-item-price">{ formatPrice(getFinalPrice(product)) }</div>
                 </div>
-                <h2>{product.name}</h2>
-                <p>{ product.type }</p>
-                <div className="cart-popup-item-price">{ getFinalPrice(product) }</div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+            <div className="cart-popup-totals">
+              Subtotal: { formatPrice(getSubotal()) }<br />
+              Tax: { formatPrice(getTax()) }<br />
+              <strong>TOTAL: { formatPrice(getGrandTotal()) }</strong>
+            </div>
+            <div className="cart-popup-checkout">
+              <button
+                className="buy-button continue-button"
+                onClick={() => {
+                  dispatch(closePopup())
+                }}
+              >
+                Continue shopping
+              </button>
+              <button
+                className="buy-button"
+                onClick={checkoutOnClick}
+              >
+                Checkout
+              </button>
+            </div>
+          </>
         )}
       </div>
     </section>
