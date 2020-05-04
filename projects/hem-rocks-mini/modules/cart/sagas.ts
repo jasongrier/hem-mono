815566747eb1874b99c1845b5b2215793d35aa7f
Jason Grier <jason@hemberlin.de>
2020-05-04 20:18:50 +0200

    Mini: Adapt to Shopify digital downloads app limitations

--- projects/hem-rocks-mini/modules/cart/sagas.ts
+++ projects/hem-rocks-mini/modules/cart/sagas.ts
@@ -3,14 +3,13 @@ import {
   SHOPIFY_CHECK_OUT,
 } from './index'
 
-declare const NODE_ENV: string
-
 function* shopifyCheckOut({ payload }: any) {
   const { productHandles, prices } = payload
 
   try {
     const data = yield call(() => {
-      let appUrl = `http://localhost:8888/pricing.php?product_handles=${productHandles.join()}&prices=${prices.join()}`
+      const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8888/' : 'http://shopify-app.hem.rocks/'
+      let appUrl = `${baseUrl}pricing.php?product_handles=${productHandles.join()}&prices=${prices.join()}`
 
       if (process.env.NODE_ENV === 'development') {
         appUrl += '&dev_mode=true'
@@ -38,7 +37,7 @@ function* shopifyCheckOut({ payload }: any) {
       location += 'https://hem-rocks.myshopify.com/cart/'
     }
 
-    location += data.variants.map((variant: any) => variant.id + ':1').join()
+    location += data.ids.map((id: string) => id + ':1').join()
 
     window.location.href = location
 
