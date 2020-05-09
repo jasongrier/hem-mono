c0055ab75e63d3688427bc21d4ba7bbd1b0f5318
Jason Grier <jason@hemberlin.de>
2020-05-09 22:04:00 +0200

    Mini: Kinda got silly PPL button working

--- projects/hem-rocks-mini/modules/cart/components/CartPopup.tsx
+++ projects/hem-rocks-mini/modules/cart/components/CartPopup.tsx
@@ -2,11 +2,11 @@ import React, { ReactElement, useCallback, useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import ReactGA from 'react-ga'
 import { CloseButton } from '../../../../../lib/packages/hem-buttons'
+import Scrollbars from 'react-scrollbars-custom'
 import { IContentItem } from '../../content'
 import { RootState } from '../../../index'
-import { removeProductFromCart, shopifyCheckOut } from '../actions'
+import { removeProductFromCart } from '../actions'
 import { closePopup } from '../../../../../lib/modules/popups'
-import { Spinner } from '../../../../../lib/components'
 
 declare const paypal: any
 
@@ -20,23 +20,36 @@ function CartPopup(): ReactElement {
   const [redirecting, setRedirecting] = useState(false)
 
   useEffect(function initPayPal() {
-    paypal.Buttons().render('#paypal-button-container')
-  }, [])
+    if (!redirecting) return
+    paypal.Buttons({
+      createOrder: function(data: any, actions: any) {
+        var itemsData = ({name: "Basic Class Fee", unit_amount: {currency_code: "EUR",value: "50.00"},quantity: "1"})
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              currency_code: "EUR",
+              value: "50.00",
+              breakdown: {
+                item_total: {
+                  currency_code: "EUR",
+                  value: "50.00"
+                }
+              }
+            },
+            items: [(itemsData)]
+          }]
+        })
+      }
+    }).render('#paypal-button-container')
+  }, [redirecting])
 
   const checkoutOnClick = useCallback(
     function checkoutOnClickFn() {
-      dispatch(shopifyCheckOut(
-        // @ts-ignore
-        cartProducts.map(p => p.shopifyHandle),
-        // @ts-ignore
-        cartProducts.map(p => getFinalPrice(p)),
-      ))
-
       setRedirecting(true)
 
       ReactGA.event({
         category: 'User',
-        action: 'Clicked "Check out" in shopping cart for',
+        action: 'Clicked "Check out" in shopping cart form',
       })
     }, [],
   )
@@ -76,74 +89,74 @@ function CartPopup(): ReactElement {
 
   return (
     <section className="cart-popup">
-      <header>
-        <h1>Shopping cart</h1>
-      </header>
-      <div className="cart-popup-content">
-        {!!!cartProducts.length && (
-          <div className="cart-popup-empty">
-            Your cart is empty.
-          </div>
-        )}
-        {!!cartProducts.length && (
-          <>
-            <div className="cart-popup-items">
-              {cartProducts.map(product => (
-                <div
-                  className="cart-popup-item"
-                  key={product.id}
-                >
-                  <div className="cart-popup-item-remove">
-                    <CloseButton
-                      onClick={() => {
-                        ReactGA.event({
-                          category: 'User',
-                          action: 'Clicked "remove" in shopping cart for ' + product.name,
-                        })
-                        dispatch(removeProductFromCart(product.id))
-                      }}
-                    />
+      <Scrollbars noScrollX={true}>
+        <header>
+          <h1>Shopping cart</h1>
+        </header>
+        <div className="cart-popup-content">
+          {!!!cartProducts.length && (
+            <div className="cart-popup-empty">
+              Your cart is empty.
+            </div>
+          )}
+          {!!cartProducts.length && (
+            <>
+              <div className="cart-popup-items">
+                {cartProducts.map(product => (
+                  <div
+                    className="cart-popup-item"
+                    key={product.id}
+                  >
+                    <div className="cart-popup-item-remove">
+                      <CloseButton
+                        onClick={() => {
+                          ReactGA.event({
+                            category: 'User',
+                            action: 'Clicked "remove" in shopping cart for ' + product.name,
+                          })
+                          dispatch(removeProductFromCart(product.id))
+                        }}
+                      />
+                    </div>
+                    <h2>{product.name}</h2>
+                    <p>{ product.type }</p>
+                    <div className="cart-popup-item-price">{ formatPrice(getFinalPrice(product)) }</div>
                   </div>
-                  <h2>{product.name}</h2>
-                  <p>{ product.type }</p>
-                  <div className="cart-popup-item-price">{ formatPrice(getFinalPrice(product)) }</div>
-                </div>
-              ))}
+                ))}
+              </div>
+              <div className="cart-popup-totals">
+                Subtotal: { formatPrice(getSubotal()) }<br />
+                Tax: { formatPrice(getTax()) }<br />
+                <strong>TOTAL: { formatPrice(getGrandTotal()) }</strong>
+              </div>
+              <div className="cart-popup-check-out">
+                <button
+                  className="action-button continue-button"
+                  onClick={() => {
+                    dispatch(closePopup())
+                  }}
+                >
+                  Continue shopping
+                </button>
+                <button
+                  className="action-button"
+                  onClick={checkoutOnClick}
+                >
+                  Check out
+                </button>
+              </div>
+            </>
+          )}
+          {redirecting && (
+            <div className="cart-popup-redirecting-overlay">
+              <div className="cart-popup-redirecting-overlay-content">
+                <h2>Choose payment method</h2>
+                <div id="paypal-button-container"></div>
+              </div>
             </div>
-            <div className="cart-popup-totals">
-              Subtotal: { formatPrice(getSubotal()) }<br />
-              Tax: { formatPrice(getTax()) }<br />
-              <strong>TOTAL: { formatPrice(getGrandTotal()) }</strong>
-            </div>
-            <div className="cart-popup-check-out">
-              <button
-                className="action-button continue-button"
-                onClick={() => {
-                  dispatch(closePopup())
-                }}
-              >
-                Continue shopping
-              </button>
-              <button
-                className="action-button"
-                onClick={checkoutOnClick}
-              >
-                Check out
-              </button>
-              <div id="paypal-button-container"></div>
-            </div>
-          </>
-        )}
-        {redirecting && (
-          <div className="cart-popup-redirecting-overlay">
-            <div className="cart-popup-redirecting-overlay-content">
-              <h2>Yay!</h2>
-              <p>Checkin' you out now...</p>
-              <Spinner />
-            </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </Scrollbars>
     </section>
   )
 }
