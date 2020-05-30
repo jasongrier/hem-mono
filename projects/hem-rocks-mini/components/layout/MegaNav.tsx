8ad876ef2d9d47a7e4645ac9eafd8020a78ddf2f
Jason Grier <jason@hemberlin.de>
2020-05-30 13:34:28 +0200

    Mini: Mega nav

--- projects/hem-rocks-mini/components/layout/MegaNav.tsx
+++ projects/hem-rocks-mini/components/layout/MegaNav.tsx
@@ -7,54 +7,74 @@ function MegaNav(): ReactElement {
     <nav className="mega-nav">
       <HamburgerMenu openByDefault={true}>
         <Logo />
-        <div className="clearfix">
+        <div className="mega-nav-sections clearfix">
           {/* Products */}
-          <ul className="mega-nav-section">
-            <MainNavItem name="Apps" />
-            <MainNavItem name="Sound Library" />
-            <MainNavItem name="Label" />
-            <MainNavItem name="Merch" />
-          </ul>
+          <div className="mega-nav-section">
+            <h3>Products</h3>
+            <ul>
+              <MainNavItem name="Apps" />
+              <MainNavItem name="Sound Library" />
+              <MainNavItem name="Label" />
+              <MainNavItem name="Merch" />
+            </ul>
+          </div>
 
           {/* Venue */}
-          <ul className="mega-nav-section">
-            <MainNavItem name="Calendar" />
-            <MainNavItem name="Archive" />
-          </ul>
+          <div className="mega-nav-section">
+            <h3>Venue</h3>
+            <ul>
+              <MainNavItem name="Calendar" />
+              <MainNavItem name="Archive" />
+            </ul>
+          </div>
 
           {/* Media */}
-          <ul className="mega-nav-section">
-            <MainNavItem name="Mixes" />
-            <MainNavItem name="Tracks" />
-            <MainNavItem name="Videos" />
-          </ul>
+          <div className="mega-nav-section">
+            <h3>Media</h3>
+            <ul>
+              <MainNavItem name="Mixes" />
+              <MainNavItem name="Tracks" />
+              <MainNavItem name="Videos" />
+            </ul>
+          </div>
 
           {/* Info */}
-          <ul className="mega-nav-section">
-            <MainNavItem name="Info" />
-            <MainNavItem name="Press" />
-            <MainNavItem name="Blog" />
-            <MainNavItem name="Code" />
-            <MainNavItem name="Mailing List" />
-          </ul>
+          <div className="mega-nav-section">
+            <h3>Info</h3>
+            <ul>
+              <MainNavItem name="Info" />
+              <MainNavItem name="Press" />
+              <MainNavItem name="Blog" />
+              <MainNavItem name="Code" />
+              <MainNavItem name="Mailing List" />
+            </ul>
+          </div>
 
           {/* Support */}
-          <ul className="mega-nav-section">
-            <MainNavItem name="Support Home" />
-            <MainNavItem name="Tutorials" />
-            <MainNavItem name="User Guides" />
-          </ul>
+          <div className="mega-nav-section">
+            <h3>Support</h3>
+            <ul>
+              <MainNavItem name="Support" />
+              <MainNavItem name="FAQ's" />
+              <MainNavItem name="Tutorials" />
+              <MainNavItem name="User Guides" />
+              <MainNavItem name="Contact" />
+            </ul>
+          </div>
 
           {/* Admin */}
-          <ul className="mega-nav-section">
-            <MainNavItem name="Privacy Policy" />
-            <MainNavItem name="Cookie Policy" />
-            <MainNavItem name="Impressum" />
-            <MainNavItem name="Set cookie preferences" to="set-cookie-preferences" />
-            <ElectronOnly>
-              <MainNavItem name="Admin" to="admin/list" />
-            </ElectronOnly>
-          </ul>
+          <div className="mega-nav-section">
+            <h3>Etc.</h3>
+            <ul>
+              <MainNavItem name="Privacy Policy" />
+              <MainNavItem name="Cookie Policy" />
+              <MainNavItem name="Impressum" />
+              <MainNavItem name="Cookie preferences" to="set-cookie-preferences" />
+              <ElectronOnly>
+                <MainNavItem name="Admin" to="admin/list" />
+              </ElectronOnly>
+            </ul>
+          </div>
         </div>
       </HamburgerMenu>
     </nav>
