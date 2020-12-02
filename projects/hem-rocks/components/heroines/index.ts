1a0f21992a25e65e5a812b1ac886f92202bf20eb
Jason Grier <jason@hemberlin.de>
2020-12-02 18:17:42 +0100

    HEM: Start final content

--- projects/hem-rocks/components/heroines/index.ts
+++ projects/hem-rocks/components/heroines/index.ts
@@ -1,2 +1,3 @@
 export { default as GrandPianoHeroine } from './GrandPianoHeroine'
+export { default as HemRefreshHeroine } from './HemRefreshHeroine'
 export { default as SoundLibraryRefreshHeroine } from './SoundLibraryRefreshHeroine'
