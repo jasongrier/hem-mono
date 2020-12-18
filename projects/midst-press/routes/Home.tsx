3d858edaa8cc6a4ee33af828929a350daea7bf77
Annelyse Gelman <annelyse.robin@gmail.com>
2020-12-17 16:56:56 -0800

    Please fix smiley bug in TOC

--- projects/midst-press/routes/Home.tsx
+++ projects/midst-press/routes/Home.tsx
@@ -3,56 +3,10 @@ import { Helmet } from 'react-helmet'
 import { BASE_SITE_TITLE } from '../config'
 
 const randomNames = [
-  'Carylon Huie',
-  'Hedwig Lloyd',
-  'Apolonia Joye',
-  'David Pittsley',
-  'Edwina Eppler',
-  'Maricruz Swanberg',
-  'Jeannie Balbuena',
-  'Adolph Riendeau',
-  'Mabelle Bugarin',
-  'Sophie Zumwalt',
-  'Benita Cowles',
-  'Domenica Reif',
-  'Virgie Crass',
-  'Cherry Dobrowolski',
-  'Bennett Funston',
-  'Alyse Mortellaro',
-  'Luise Snipes',
-  'Charlyn Mance',
-  'Yuki Shea',
-  'Shannan Gholston',
-  'Lilly Barrios',
-  'Fredericka Yip',
-  'Kurtis Dezern',
-  'Julissa Demeo',
-  'Hortensia Sansbury',
-  'Eleanore Muldowney',
-  'Amberly Cosner',
-  'Tawanda Trumbauer',
-  'Waneta Obregon',
-  'Loura Round',
-  'Michelle Govan',
-  'Marx Schwartzkopf',
-  'Blossom Evert',
-  'Ardelia Murphy',
-  'Awilda Pan',
-  'Venetta Sung',
-  'Christin Ayres',
-  'Caren Clement',
-  'Dario Walworth',
-  'Kristel Mendelson',
-  'Chandra Salvatore',
-  'Lyman Moretti',
-  'Ronna Spadaro',
-  'Sima Wrona',
-  'Vickey Somers',
-  'Antwan Goyette',
-  'Nicholle Collett',
-  'Tanna Bax',
-  'Marlena Yan',
-  'Jetta Eakin',
+  'Juliet Shafto',
+  'Myrrh Crow',
+  'Lara Prescott',
+  'Alejandro Puyana', 
 ]
 
 function Home(): ReactElement {
@@ -72,10 +26,14 @@ function Home(): ReactElement {
           Midst is a new digital journal publishing poems in the form of interactive timelapses. You’ll see the finished text by default, but then you can rewind it to see exactly how it was written: start to finish, blank page to final draft, and every edit in between.
         </p>
 
+        <p style={{ paddingTop: '20px' }}>
+          <a href="http://www.midst.press/read">Go forth to the poems!</a>
+        </p>
+
         <p style={{ paddingTop: '10px' }} />
 
         <div className="donor-wall">
-          <h2>With special thanks to:</h2>
+          <h2>Special thanks to our <a href="http://www.patreon.com/midstpoetry">Patrons</a>:</h2>
           <ul>
             {randomNames.map(name => (
               <li key={name}>{ name }</li>
