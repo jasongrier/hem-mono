000e1ac8d7dab7608fc8dddf5df470a560b181a6
Jason Grier <jason@hemberlin.de>
2020-12-23 10:42:26 +0100

    HEM: WebMovie modelize

--- lib/modules/web-movie/functions/modelize.ts
+++ lib/modules/web-movie/functions/modelize.ts
@@ -1,4 +1,18 @@
-function someFunction() {
+import { IWebMovie, IWebMovieClip, IWebMovieFrame } from '../index'
+
+function modelize(src: string, movieInfo: any): IWebMovie {
+  return {
+    clips: movieInfo.clips.map(clipInfo => ({
+      frames: clipInfo.frameFileNames.map(fileName => ({
+        loaded: false,
+        fileName,
+      } as IWebMovieFrame)),
+      name: clipInfo.name,
+    } as IWebMovieClip)),
+    slug: movieInfo.slug,
+    src,
+    title: movieInfo.title,
+  }
 }
 
-export default someFunction
+export default modelize
