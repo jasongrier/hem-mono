5e43b8d093425e484a09593cf9c5564aaec8447f
Jason Grier <jason@hemberlin.de>
2021-02-15 23:15:02 +0100

    JAG: Clean slate

--- projects/jag-rip/index.tsx
+++ projects/jag-rip/index.tsx
@@ -5,27 +5,13 @@ import { BrowserRouter } from 'react-router-dom'
 import { combineReducers, createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { popupsReducer } from '../../lib/modules/popups'
-import { playerReducer } from '../../lib/modules/website-player'
-import { webMovieReducer } from '../../lib/modules/web-movie'
-import { flipBooksReducer } from '../../lib/modules/flip-book'
 import { App, appReducer } from './modules/app'
-import { loginReducer } from './modules/login'
-import { cartReducer } from './modules/cart'
-import { contentReducer } from './modules/content'
 import './styles'
 
 declare const window: any
 
 const rootReducer = combineReducers({
   app: appReducer,
-  cart: cartReducer,
-  content: contentReducer,
-  flipBooks: flipBooksReducer,
-  login: loginReducer,
-  player: playerReducer,
-  popups: popupsReducer,
-  webMovie: webMovieReducer,
 })
 
 const sagaMiddleware = createSagaMiddleware()
@@ -37,77 +23,8 @@ const store = window.STORE = createStore(
   )
 )
 
-import { requestActiveLiveStreamSaga } from './modules/app'
-sagaMiddleware.run(requestActiveLiveStreamSaga)
-
-import { nextTrackSaga } from '../../lib/modules/website-player'
-sagaMiddleware.run(nextTrackSaga)
-
-import { pausePlayerSaga } from '../../lib/modules/website-player'
-sagaMiddleware.run(pausePlayerSaga)
-
-import { playPlayerSaga } from '../../lib/modules/website-player'
-sagaMiddleware.run(playPlayerSaga)
-
-import { previousTrackSaga } from '../../lib/modules/website-player'
-sagaMiddleware.run(previousTrackSaga)
-
-import { seekPlayerSaga } from '../../lib/modules/website-player'
-sagaMiddleware.run(seekPlayerSaga)
-
-import { setPlayerInstanceSaga } from '../../lib/modules/website-player'
-sagaMiddleware.run(setPlayerInstanceSaga)
-
-import { trackEndedSaga } from '../../lib/modules/website-player'
-sagaMiddleware.run(trackEndedSaga)
-
-import { unpausePlayerSaga } from '../../lib/modules/website-player'
-sagaMiddleware.run(unpausePlayerSaga)
-
-import { createItemsSaga } from './modules/content'
-sagaMiddleware.run(createItemsSaga)
-
-import { readChunkSaga } from './modules/content'
-sagaMiddleware.run(readChunkSaga)
-
-import { readItemsSaga } from './modules/content'
-sagaMiddleware.run(readItemsSaga)
-
-import { updateItemsSaga } from './modules/content'
-sagaMiddleware.run(updateItemsSaga)
-
-import { deleteItemsSaga } from './modules/content'
-sagaMiddleware.run(deleteItemsSaga)
-
-import { addProductToCartSaga } from './modules/cart'
-sagaMiddleware.run(addProductToCartSaga)
-
-import { clearCartSaga } from './modules/cart'
-sagaMiddleware.run(clearCartSaga)
-
-import { removeProductFromCartSaga } from './modules/cart'
-sagaMiddleware.run(removeProductFromCartSaga)
-
-import { requestSaleSaga } from './modules/cart'
-sagaMiddleware.run(requestSaleSaga)
-
-import { submitSaleSaga } from './modules/cart'
-sagaMiddleware.run(submitSaleSaga)
-
-import { loginCheckSaga } from './modules/login'
-sagaMiddleware.run(loginCheckSaga)
-
-import { loginSaga } from './modules/login'
-sagaMiddleware.run(loginSaga)
-
-import { logoutSaga } from './modules/login'
-sagaMiddleware.run(logoutSaga)
-
-import { flipBooksSaga } from '../../lib/modules/flip-book'
-sagaMiddleware.run(flipBooksSaga)
-
-import { webMovieSaga } from '../../lib/modules/web-movie'
-sagaMiddleware.run(webMovieSaga)
+import { someSaga } from './modules/app'
+sagaMiddleware.run(someSaga)
 
 const Root = (
   <Provider store={store}>
--- projects/jag-rip/pug.config.js
+++
@@ -1,8 +0,0 @@
-const config = require('./config')
-
-module.exports = {
-  locals: {
-    title: config.BASE_SITE_TITLE,
-    description: config.META_DESCRIPTION,
-  }
-}
