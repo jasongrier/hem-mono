17eb0ef82af6b1c4559f2fa653a3033327bd53d2
Jason Grier <jason@hemberlin.de>
2021-02-17 07:19:28 +0100

    JAG: Make it a subsite of HEM

--- projects/hem-rocks/routes/internal/Notes.tsx
+++ projects/hem-rocks/routes/internal/Notes.tsx
@@ -1,11 +1,9 @@
 import React, { ReactElement } from 'react'
-import { Midst } from '../../modules/midst'
 
 function Notes(): ReactElement {
   return (
     <div className="page page-internal page-internal-notes">
       <h2>Notes</h2>
-      <Midst />
     </div>
   )
 }
--- projects/jag-rip/modules/app/actions.ts
+++
@@ -1,9 +0,0 @@
-import { AnyAction } from 'redux'
-import { SOME_ACTION } from './index'
-
-const someAction = (foo: string): AnyAction => ({
-  type: SOME_ACTION,
-  payload: foo,
-})
-
-export { someAction }
