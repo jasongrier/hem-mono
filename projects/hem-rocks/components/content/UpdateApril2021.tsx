0c56c3d2caf75e85d9db93d119ab084243d1badd
Jason Grier <jason@hemberlin.de>
2021-03-17 22:18:11 +0100

    MONO: Links to list pages in component content do not close detail popup WIP

--- projects/hem-rocks/components/content/UpdateApril2021.tsx
+++ projects/hem-rocks/components/content/UpdateApril2021.tsx
@@ -1,52 +1,48 @@
 import React, { ReactElement } from 'react'
+import { Link } from 'react-router-dom'
 
 function UpdateApril2021(): ReactElement {
   return (
     <div className="new-website-content">
-      ## Articles
+      <h2>Articles</h2>
 
-      In [unpublished notes](/articles/detail/words-before-music-vito-acconci), the late Vito Acconci muses on music's functional yet immersive nature, writing from the perspective of a studio laborer, music consumer, artist, and architect.
+      <p>In <Link to="/articles/detail/words-before-music-vito-acconci">unpublished notes</Link>, the late Vito Acconci muses on music's functional yet immersive nature, writing from the perspective of a studio laborer, music consumer, artist, and architect.</p>
 
-      Grammy-nominated violinist India Cooke meets up with student and close friend Anna Luisa Petrisko (Jeepneys) for this [fascinating chat](/articles/detail/india-cooke-interviewed-by-jeepneys) about how Sun Ra saved the life of her beloved cat, Sri.
+      <p>Grammy-nominated violinist India Cooke meets up with student and close friend Anna Luisa Petrisko (Jeepneys) for this <Link to="/articles/detail/india-cooke-interviewed-by-jeepneys">fascinating chat</Link> about how Sun Ra saved the life of her beloved cat, Sri.</p>
 
-      ## Tracks
+      <h2>Tracks</h2>
 
-      Michael Pisaro and Julia Holter unveil a [touching unreleased setting](/tracks/detail/some-kind-of-emergency-feat-julia-holter-michael-pisaro) of Oswald Egger employing Casio synths and field recordings.
+      <p>Michael Pisaro and Julia Holter unveil a <Link to="/tracks/detail/some-kind-of-emergency-feat-julia-holter-michael-pisaro">touching unreleased setting</Link> of Oswald Egger employing Casio synths and field recordings.</p>
 
-      Line Gøttsche contributes the first in a selection of early breakthroughs, a moonlit piano-and-vocals track called ["June"](/tracks/detail/june-line-goettsche).
+      <p>Line Gøttsche contributes the first in a selection of early breakthroughs, a moonlit piano-and-vocals track called "<Link to="/tracks/detail/june-line-goettsche">June</Link>".</p>
 
-      Lucrecia Dalt's contribution to the shelved fifth HEM label compilation finally sees the light of day. It is an inviting trek through a deep purple atmosphere cloaked in mytery; [an expansive three-and-a-half minutes](/tracks/detail/silent-bodies-fast-food-lucrecia-dalt).
+      <p>Lucrecia Dalt's contribution to the shelved fifth HEM label compilation finally sees the light of day. It is an inviting trek through a deep purple atmosphere cloaked in mytery; <Link to="/tracks/detail/silent-bodies-fast-food-lucrecia-dalt">an expansive three-and-a-half minutes</Link>.</p>
 
-      From the archive surfaces [ultra-rare concert audio](/playlists/detail/trauermusik-festival/featured) featuring Viennese performance artist Sara Galaxia and LA gallerist Janet Kim live at HEM's 2008 Los Angeles festival, "Trauermusik". Both appear as members of "Spider Babies", an LA pop underground supergroup which Galaxia spontaneously joined as a heckler fending off some mansplaining about her official place of residence.
+      <p>From the archive surfaces <Link to="/playlists/detail/trauermusik-festival/featured">ultra-rare concert audio</Link> featuring Viennese performance artist Sara Galaxia and LA gallerist Janet Kim live at HEM's 2008 Los Angeles festival, "Trauermusik". Both appear as members of "Spider Babies", an LA pop underground supergroup which Galaxia spontaneously joined as a heckler fending off some mansplaining about her official place of residence.</p>
 
-      Also from this festival is a recording of Julia Holter performing an early version of ["The Falling Age"](/tracks/detail/the-falling-age-live-at-trauermusik), which would appear a few years later on 2011's release "Tragedy".
+      <p>Also from this festival is a recording of Julia Holter performing an early version of <Link to="/tracks/detail/the-falling-age-live-at-trauermusik">The Falling Age</Link>, which would appear a few years later on 2011's release "Tragedy".</p>
 
-      Previously released Trauermusik tracks by [William Basinski](/tracks/detail/untitled-william-basinski) and HEM director [Jason Grier](/tracks/detail/conversation-piece-3-jason-grier-jag) resurface along with hundreds of other tracks from HEM's archive, all streaming exclusively in the [__HEM Jukebox__](/tracks).
+      <p>Previously released Trauermusik tracks by <Link to="/tracks/detail/untitled-william-basinski">William Basinski</Link> and HEM director <Link to="/tracks/detail/conversation-piece-3-jason-grier-jag">Jason Grier</Link> resurface along with hundreds of other tracks from HEM's archive, all streaming exclusively in the <Link to="/tracks">__HEM Jukebox__</Link>.</p>
 
-      ## Sound Library
+      <h2>Sound Library</h2>
 
-      [SL2](/sound-library) is a significant update to the best of the original SL1 packs. Sounds fill the keys, new parameters allow real-time tweaking. The focus is on playability and free improvisation.
+      <p><Link to="/sound-library">SL2</Link> is a significant update to the best of the original SL1 packs. Sounds fill the keys, new parameters allow real-time tweaking. The focus is on playability and free improvisation.</p>
 
-      SL2's sound design ethos is rooted in acousmatics, found sound, sonic detritus, extended technique, documentary, and audio forensics. SL2 packs are meant to be _played_, with full keyboard spreads, dynamics control, and tactile parameters. Each SL2 pack is sourced from sounds chosen for more than just design concerns, like ecomusicology, labor, activism, artistic research.
+      <p>SL2's sound design ethos is rooted in acousmatics, found sound, sonic detritus, extended technique, documentary, and audio forensics. SL2 packs are meant to be _played_, with full keyboard spreads, dynamics control, and tactile parameters. Each SL2 pack is sourced from sounds chosen for more than just design concerns, like ecomusicology, labor, activism, artistic research.</p>
 
-      ## Editions
+      <h2>Editions</h2>
 
-      Select tracks and audio ephemera from the archive are now available as exclusive editions in the [_NFT gallery_](/editions).
+      <p>Select tracks and audio ephemera from the archive are now available as exclusive editions in the <Link to="/editions">_NFT gallery_</Link>.</p>
 
-      ## Exhibits
+      <h2>Exhibits</h2>
 
-      A new pictorial-essay format, called "Exhibits", HEM director Jason Grier explores the [multiple forms](/exhibits/detail/the-catalogue-as-a-work-of-art-exhibit/new) that the catalogue-for-catalogue's-sake can take within, and as, a range of practices and forms.
+      <p>A new pictorial-essay format, called "Exhibits", HEM director Jason Grier explores the <Link to="/exhibits/detail/the-catalogue-as-a-work-of-art-exhibit/new">multiple forms</Link> that the catalogue-for-catalogue's-sake can take within, and as, a range of practices and forms.</p>
 
-      ## HEM is now "HEM"
+      <h2>HEM is now "HEM"</h2>
 
-      HEM is short for Hot Extramusicality. It has inherited the vast archive of original sound artworks, album-oriented sound art, rare tracks, live recordings, demos, outtakes, sketches, and album releases from the vaunted —and now defunct— record label "Human Ear Music" (HEM), and continues to expand on this collection to the present day.
+      <p>HEM is short for Hot Extramusicality. It has inherited the vast archive of original sound artworks, album-oriented sound art, rare tracks, live recordings, demos, outtakes, sketches, and album releases from the vaunted —and now defunct— record label "Human Ear Music" (HEM), and continues to expand on this collection to the present day.</p>
 
-      HEM is dead. Long live HEM.
-
-
-
-
-
+      <p>HEM is dead. Long live HEM.</p>
     </div>
   )
 }
